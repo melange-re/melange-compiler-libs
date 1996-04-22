8510f146020895d9a2d17f3f0722441f10e14c4f
Jérôme Vouillon <Jerome.Vouillon@pps.jussieu.fr>
1996-04-22 11:15:41 +0000

    Extension objets.
    git-svn-id: http://caml.inria.fr/svn/ocaml/trunk@756 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- parsing/label.ml
+++ parsing/label.ml
@@ -2,26 +2,19 @@
 (*                                                                     *)
 (*                         Caml Special Light                          *)
 (*                                                                     *)
-(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*         Jerome Vouillon, projet Cristal, INRIA Rocquencourt         *)
 (*                                                                     *)
-(*  Copyright 1995 Institut National de Recherche en Informatique et   *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
 (*  Automatique.  Distributed only by permission.                      *)
 (*                                                                     *)
 (***********************************************************************)
 
 (* $Id$ *)
 
-(* Inclusion checks for the core language *)
+type t = { lab_name : string }
 
-open Typedtree
+let compare l1 l2 = compare l1.lab_name l2.lab_name
 
-exception Dont_match
+let (=) l1 l2 = l1.lab_name = l2.lab_name
 
-val value_descriptions:
-        Env.t -> value_description -> value_description -> module_coercion
-val type_declarations:
-        Env.t -> Ident.t -> type_declaration -> type_declaration -> bool
-val exception_declarations:
-        Env.t -> exception_declaration -> exception_declaration -> bool
-val class_type:
-      	Env.t -> class_type -> class_type -> bool
+let (<) l1 l2 = l1.lab_name < l2.lab_name
--- parsing/label.mli
+++ parsing/label.mli
@@ -2,26 +2,16 @@
 (*                                                                     *)
 (*                         Caml Special Light                          *)
 (*                                                                     *)
-(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*         Jerome Vouillon, projet Cristal, INRIA Rocquencourt         *)
 (*                                                                     *)
-(*  Copyright 1995 Institut National de Recherche en Informatique et   *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
 (*  Automatique.  Distributed only by permission.                      *)
 (*                                                                     *)
 (***********************************************************************)
 
 (* $Id$ *)
 
-(* Inclusion checks for the core language *)
-
-open Typedtree
-
-exception Dont_match
-
-val value_descriptions:
-        Env.t -> value_description -> value_description -> module_coercion
-val type_declarations:
-        Env.t -> Ident.t -> type_declaration -> type_declaration -> bool
-val exception_declarations:
-        Env.t -> exception_declaration -> exception_declaration -> bool
-val class_type:
-      	Env.t -> class_type -> class_type -> bool
+type t = { lab_name : string }
+val compare : t -> t -> int
+val (=) : t -> t -> bool
+val (<) : t -> t -> bool
