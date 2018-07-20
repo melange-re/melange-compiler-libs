5dcbc41329bf2cc96de7c918f8f33a3e49012959
Hongbo Zhang <bobzhang1988@gmail.com>
2018-07-20 09:08:14 +0800

    patch Typetexp deprecated error message

--- parsing/builtin_attributes.ml
+++ parsing/builtin_attributes.ml
@@ -84,7 +84,13 @@ let kind_and_message = function
   | _ -> None
 
 let cat s1 s2 =
-  if s2 = "" then s1 else s1 ^ "\n" ^ s2
+  if s2 = "" then s1 else
+#if undefined BS_NO_COMPILER_PATCH then 
+    if Clflags.bs_vscode then s1 ^ " " ^ s2
+    else s1 ^ "\n" ^ s2 
+#else    
+    s1 ^ "\n" ^ s2
+#end    
 
 let alert_attr x =
   match x.attr_name.txt with
