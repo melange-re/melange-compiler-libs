trefis/ocaml

matching: rename conda/condb to be self-contained (without reading the paper)

lambda/matching.ml
@@ -865,16 +865,32 @@ let is_or p =
   | Tpat_or _ -> true
   | _ -> false
 
+(* Splitting a matrix uses an or-matrix that contains or-patterns
+   (at the head of some of its rows).
+
+   The property that we want to maintain for the rows of the
+   or-matrix is that if the row p::ps is before q::qs and p is an
+   or-pattern, and v::vs matches p but not ps, then we don't need to
+   try q::qs. This is necessary because the compilation of the
+   or-pattern p will exit to a sub-matrix and never come back.
+
+   For this to hold, (p::ps) and (q::qs) must satify one of:
+   - disjointness: p and q are not compatible
+   - ordering: if p and q are compatible, ps is more general than qs
+     (this only works if the row p::ps is not guarded; otherwise the
+      guard could fail and q::qs should still be tried)
+*)
+
 (* Conditions for appending to the Or matrix *)
-let conda p q = not (may_compat p q)
+let disjoint p q = not (may_compat p q)
 
-and condb act ps qs = (not (is_guarded act)) && Parmatch.le_pats qs ps
+let safe_below (ps, act) qs = (not (is_guarded act)) && Parmatch.le_pats ps qs
 
-let or_ok p ps l =
+let safe_below_or_matrix l (q, qs) =
   List.for_all
     (function
-      | ({ pat_desc = Tpat_or _ } as q) :: qs, act ->
-          conda p q || condb act ps qs
+      | ({ pat_desc = Tpat_or _ } as p) :: ps, act_p ->
+          disjoint p q || safe_below (ps, act_p) qs
       | _ -> true)
     l
 
@@ -905,12 +921,12 @@ let insert_or_append p ps act ors no =
               (* attempt insert, for equivalent orpats with no variables *)
               let _, not_e = get_equiv q rem in
               if
-                or_ok p ps not_e
+                safe_below_or_matrix not_e (p, ps)
                 && (* check append condition for head of O *)
                    List.for_all (* check insert condition for tail of O *)
                      (fun cl ->
                        match cl with
-                       | q :: _, _ -> not (may_compat p q)
+                       | q :: _, _ -> disjoint p q
                        | _ -> assert false)
                      seen
               then
@@ -919,7 +935,7 @@ let insert_or_append p ps act ors no =
               else
                 (* fail to insert or append *)
                 (ors, (p :: ps, act) :: no)
-            else if condb act_q ps qs then
+            else if safe_below (qs, act_q) ps then
               (* check condition (b) for append *)
               attempt (cl :: seen) rem
             else
@@ -989,7 +1005,7 @@ let rec split_or argo cls args def =
             do_split before ors no rem
           else if up_ok cl ors then
             do_split (cl :: before) ors no rem
-          else if or_ok p ps ors then
+          else if safe_below_or_matrix ors (p, ps) then
             do_split before (cl :: ors) no rem
           else
             do_split before ors (cl :: no) rem

melange-re/melange-compiler-libs

lambda/matching.ml
@@ -865,16 +865,32 @@ let is_or p =
   | Tpat_or _ -> true
   | _ -> false
 
+(* Splitting a matrix uses an or-matrix that contains or-patterns
+   (at the head of some of its rows).
+
+   The property that we want to maintain for the rows of the
+   or-matrix is that if the row p::ps is before q::qs and p is an
+   or-pattern, and v::vs matches p but not ps, then we don't need to
+   try q::qs. This is necessary because the compilation of the
+   or-pattern p will exit to a sub-matrix and never come back.
+
+   For this to hold, (p::ps) and (q::qs) must satisfy one of:
+   - disjointness: p and q are not compatible
+   - ordering: if p and q are compatible, ps is more general than qs
+     (this only works if the row p::ps is not guarded; otherwise the
+      guard could fail and q::qs should still be tried)
+*)
+
 (* Conditions for appending to the Or matrix *)
-let conda p q = not (may_compat p q)
+let disjoint p q = not (may_compat p q)
 
-and condb act ps qs = (not (is_guarded act)) && Parmatch.le_pats qs ps
+let safe_below (ps, act) qs = (not (is_guarded act)) && Parmatch.le_pats ps qs
 
-let or_ok p ps l =
+let safe_below_or_matrix l (q, qs) =
   List.for_all
     (function
-      | ({ pat_desc = Tpat_or _ } as q) :: qs, act ->
-          conda p q || condb act ps qs
+      | ({ pat_desc = Tpat_or _ } as p) :: ps, act_p ->
+          disjoint p q || safe_below (ps, act_p) qs
       | _ -> true)
     l
 
@@ -905,12 +921,12 @@ let insert_or_append p ps act ors no =
               (* attempt insert, for equivalent orpats with no variables *)
               let _, not_e = get_equiv q rem in
               if
-                or_ok p ps not_e
+                safe_below_or_matrix not_e (p, ps)
                 && (* check append condition for head of O *)
                    List.for_all (* check insert condition for tail of O *)
                      (fun cl ->
                        match cl with
-                       | q :: _, _ -> not (may_compat p q)
+                       | q :: _, _ -> disjoint p q
                        | _ -> assert false)
                      seen
               then
@@ -919,7 +935,7 @@ let insert_or_append p ps act ors no =
               else
                 (* fail to insert or append *)
                 (ors, (p :: ps, act) :: no)
-            else if condb act_q ps qs then
+            else if safe_below (qs, act_q) ps then
               (* check condition (b) for append *)
               attempt (cl :: seen) rem
             else
@@ -989,7 +1005,7 @@ let rec split_or argo cls args def =
             do_split before ors no rem
           else if up_ok cl ors then
             do_split (cl :: before) ors no rem
-          else if or_ok p ps ors then
+          else if safe_below_or_matrix ors (p, ps) then
             do_split before (cl :: ors) no rem
           else
             do_split before ors (cl :: no) rem