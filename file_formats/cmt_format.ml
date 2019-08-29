e7279156acc0e6a780094ed74f52cfffd1b3c563
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-08-29 17:39:36 +0800

    removing genType calling

--- file_formats/cmt_format.ml
+++ file_formats/cmt_format.ml
@@ -190,11 +190,5 @@ let save_cmt filename modname binary_annots sourcefile initial_env cmi =
            cmt_use_summaries = need_to_clear_env;
          } in
          output_cmt oc cmt)
-#if undefined BS_NO_COMPILER_PATCH then
-    (* TODO: does not make sense to do post-proccesing for [Partial_implementaiton]*)
-  ; match !Clflags.bs_gentype with
-    | None -> ()
-    | Some cmd -> ignore (Sys.command (cmd ^ " -cmt-add " ^ filename ^ (match sourcefile with None -> "" | Some sourcefile -> ":" ^ sourcefile)))
-#end
   end;
   clear ()
