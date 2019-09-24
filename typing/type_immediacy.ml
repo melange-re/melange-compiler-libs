keleshev/ocaml

Add support for [@@immediate64]
Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

typing/type_immediacy.ml
@@ -2,11 +2,9 @@
 (*                                                                        *)
 (*                                 OCaml                                  *)
 (*                                                                        *)
-(*   Gabriel Scherer, projet Parsifal, INRIA Saclay                       *)
-(*   Rodolphe Lepigre, projet Deducteam, INRIA Saclay                     *)
+(*                   Jeremie Dimino, Jane Street Europe                   *)
 (*                                                                        *)
-(*   Copyright 2018 Institut National de Recherche en Informatique et     *)
-(*     en Automatique.                                                    *)
+(*   Copyright 2019 Jane Street Group LLC                                 *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -14,18 +12,29 @@
 (*                                                                        *)
 (**************************************************************************)
 
-type error =
-  | Bad_immediacy_attribute of {
-      written_by_user : Type_immediacy.t;
-      real : Type_immediacy.t;
-    }
-exception Error of Location.t * error
+type t =
+  | Unknown
+  | Always
+  | Always_on_64bits
 
-val compute_decl : Env.t -> Types.type_declaration -> Type_immediacy.t
+let more_often_immediate a b =
+  match a, b with
+  | Always, (Unknown | Always_on_64bits)
+  | Always_on_64bits, Unknown -> true
+  | Unknown, _
+  | Always, Always
+  | Always_on_64bits, (Always | Always_on_64bits) -> false
 
-val property : (Type_immediacy.t, unit) Typedecl_properties.property
+let describe = function
+  | Unknown -> "not an immediate type"
+  | Always -> "always an immediate type"
+  | Always_on_64bits -> "only an immediate type on 64 bit platforms"
 
-val update_decls :
-  Env.t ->
-  (Ident.t * Typedecl_properties.decl) list ->
-  (Ident.t * Typedecl_properties.decl) list
+let of_attributes attrs =
+  match
+    Builtin_attributes.immediate attrs,
+    Builtin_attributes.immediate64 attrs
+  with
+  | true, _ -> Always
+  | false, true -> Always_on_64bits
+  | false, false -> Unknown

melange-re/melange-compiler-libs

typing/type_immediacy.ml
@@ -2,11 +2,9 @@
 (*                                                                        *)
 (*                                 OCaml                                  *)
 (*                                                                        *)
-(*   Gabriel Scherer, projet Parsifal, INRIA Saclay                       *)
-(*   Rodolphe Lepigre, projet Deducteam, INRIA Saclay                     *)
+(*                   Jeremie Dimino, Jane Street Europe                   *)
 (*                                                                        *)
-(*   Copyright 2018 Institut National de Recherche en Informatique et     *)
-(*     en Automatique.                                                    *)
+(*   Copyright 2019 Jane Street Group LLC                                 *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -14,14 +12,32 @@
 (*                                                                        *)
 (**************************************************************************)
 
-type error = Bad_immediacy_attribute of Type_immediacy.Violation.t
-exception Error of Location.t * error
+type t =
+  | Unknown
+  | Always
+  | Always_on_64bits
 
-val compute_decl : Env.t -> Types.type_declaration -> Type_immediacy.t
+module Violation = struct
+  type t =
+    | Not_always_immediate
+    | Not_always_immediate_on_64bits
+end
 
-val property : (Type_immediacy.t, unit) Typedecl_properties.property
+let coerce t ~as_ =
+  match t, as_ with
+  | _, Unknown
+  | Always, Always
+  | (Always | Always_on_64bits), Always_on_64bits -> Ok ()
+  | (Unknown | Always_on_64bits), Always ->
+      Error Violation.Not_always_immediate
+  | Unknown, Always_on_64bits ->
+      Error Violation.Not_always_immediate_on_64bits
 
-val update_decls :
-  Env.t ->
-  (Ident.t * Typedecl_properties.decl) list ->
-  (Ident.t * Typedecl_properties.decl) list
+let of_attributes attrs =
+  match
+    Builtin_attributes.immediate attrs,
+    Builtin_attributes.immediate64 attrs
+  with
+  | true, _ -> Always
+  | false, true -> Always_on_64bits
+  | false, false -> Unknown