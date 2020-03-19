03b51c11cb75d9c47519a83a30f05a24ad040f0f
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-03-19 17:56:25 +0800

    Add bs.meth, bs.this printing

--- typing/oprint.ml
+++ typing/oprint.ml
@@ -296,7 +296,7 @@ and print_simple_out_type ppf =
       fprintf ppf "@[%a%s#%a@]" print_typargs tyl (if ng then "_" else "")
         print_ident id
 #if true then
-  | Otyp_constr (Oide_dot (Oide_dot (Oide_ident { printed_name = "Js" }, "Internal"), name),
+  | Otyp_constr (Oide_dot (Oide_dot (Oide_ident { printed_name = "Js" }, "Fn"), name),
                  [tyl])
     ->
       let res =
@@ -305,62 +305,19 @@ and print_simple_out_type ppf =
         else tyl
       in
           fprintf ppf "@[<0>(%a@ [@bs])@]" print_out_type_1 res
-  | Otyp_constr (Oide_dot (Oide_ident { printed_name = "Js_internalOO" }, "meth" ) as id ,
-                 ([Otyp_variant(_,Ovar_fields [ variant, _, tys], _,_); result] as tyl))
+  | Otyp_constr (Oide_dot (Oide_dot (Oide_ident { printed_name = "Js_OO" }, "Meth" ),name),
+        [tyl])
     ->
-      (* Otyp_arrow*)
-      let make tys result =
-        if tys = [] then
-          Otyp_arrow ("", Otyp_constr (Oide_ident { printed_name = "unit" }, []),result)
-        else
-          match tys with
-          | [ Otyp_tuple tys as single] ->
-              if variant = "Arity_1" then
-                Otyp_arrow ("", single, result)
-              else
-                List.fold_right (fun x acc  -> Otyp_arrow("",x,acc) ) tys result
-          | [single] ->
-              Otyp_arrow ("", single, result)
-          | _ ->
-              raise_notrace Not_found
+      let res =
+        if name = "arity0" then
+          Otyp_arrow ("", Otyp_constr (Oide_ident { printed_name = "unit" }, []),tyl)
+        else tyl
       in
-      begin match (make tys result) with
-      | exception _ ->
-          begin
-            pp_open_box ppf 0;
-            print_typargs ppf tyl;
-            print_ident ppf id;
-            pp_close_box ppf ()
-          end
-      | res ->
-          fprintf ppf "@[<0>(%a@ [@bs.meth])@]" print_out_type_1 res
-      end
-  | Otyp_constr (Oide_dot (Oide_ident { printed_name = "Js_internalOO" }, "meth_callback" ) as id ,
-                 ([Otyp_variant(_,Ovar_fields [ variant, _, tys], _,_); result] as tyl))
+      fprintf ppf "@[<0>(%a@ [@bs.meth])@]" print_out_type_1 res
+  | Otyp_constr (Oide_dot (Oide_dot (Oide_ident { printed_name = "Js_OO" }, "Callback" ), _),
+                 [tyl])
     ->
-      let make tys result =
-          match tys with
-          | [ Otyp_tuple tys as single ] ->
-              if variant = "Arity_1" then Otyp_arrow ("", single, result)
-              else
-                List.fold_right (fun x acc  -> Otyp_arrow("",x,acc) ) tys result
-          | [single] ->
-              Otyp_arrow ("", single, result)
-          | _ ->
-              raise_notrace Not_found
-      in
-      begin match (make tys result) with
-      | exception _ ->
-          begin
-            pp_open_box ppf 0;
-            print_typargs ppf tyl;
-            print_ident ppf id;
-            pp_close_box ppf ()
-          end
-      | res ->
-          fprintf ppf "@[<0>(%a@ [@bs.this])@]" print_out_type_1 res
-
-      end
+      fprintf ppf "@[<0>(%a@ [@bs.this])@]" print_out_type_1 tyl
 #end
   | Otyp_constr (id, tyl) ->
       pp_open_box ppf 0;
