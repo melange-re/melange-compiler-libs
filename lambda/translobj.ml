9289e6ccd8ecb7f164692d7de96ec347141a1973
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-04-17 19:02:40 +0800

    Fix camlinternalOO.Cons meta info

--- lambda/translobj.ml
+++ lambda/translobj.ml
@@ -178,7 +178,7 @@ let oo_wrap env req f x =
            List.fold_left
              (fun lambda id ->
                 Llet(StrictOpt, Pgenval, id,
-                     Lprim(Pmakeblock(0, Blk_constructor { name = "Cons"; num_nonconst = 1}, Mutable, None),
+                     Lprim(Pmakeblock(0, Blk_record_inlined { name = "Cons"; num_nonconst = 1; fields = [|"key";"data";"next"|]}, Mutable, None),
                            [lambda_unit; lambda_unit; lambda_unit],
                            Loc_unknown),
                      lambda))
