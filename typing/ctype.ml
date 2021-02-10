melange-re/melange-compiler-libs

commit f5b2609d0e229b632001b947c0d84f66c654ca0a

Make type_expr private (#9994)

typing/ctype.ml
@@ -569,9 +569,8 @@ let really_closed = ref None
  *)
 let rec free_vars_rec real ty =
   let ty = repr ty in
-  if ty.level >= lowest_level then begin
-    ty.level <- pivot_level - ty.level;
-    begin match ty.desc, !really_closed with
+  if try_mark_node ty then
+    match ty.desc, !really_closed with
       Tvar _, _ ->
         free_variables := (ty, real) :: !free_variables
     | Tconstr (path, tl, _), Some env ->
@@ -596,8 +595,6 @@ let rec free_vars_rec real ty =
         if not (static_row row) then free_vars_rec false row.row_more
     | _    ->
         iter_type_expr (free_vars_rec true) ty
-    end;
-  end
 
 let free_vars ?env ty =
   free_variables := [];
@@ -685,7 +682,7 @@ let closed_class params sign =
     (fun (lab, _, ty) -> if lab = dummy_method then mark_type ty)
     fields;
   try
-    mark_type_node (repr sign.csig_self);
+    ignore (try_mark_node (repr sign.csig_self));
     List.iter
       (fun (lab, kind, ty) ->
         if field_kind_repr kind = Fpresent then
@@ -730,8 +727,10 @@ let duplicate_class_type ty =
 *)
 let rec generalize ty =
   let ty = repr ty in
+  (* generalize the type iff ty.level <= !current_level *)
   if (ty.level > !current_level) && (ty.level <> generic_level) then begin
     set_level ty generic_level;
+    (* recur into abbrev for the speed *)
     begin match ty.desc with
       Tconstr (_, _, abbrev) ->
         iter_abbrev generalize !abbrev
@@ -817,7 +816,7 @@ let rec normalize_package_path env p =
 let rec check_scope_escape env level ty =
   let mark ty =
     (* Mark visited types with [ty.level < lowest_level]. *)
-    set_level ty (pivot_level - ty.level)
+    set_level ty (lowest_level - 1)
   in
   let ty = repr ty in
   (* If the type hasn't been marked, check it. Otherwise, we have already
@@ -1088,17 +1087,14 @@ let compute_univars ty =
 
 
 let fully_generic ty =
-  let rec aux acc ty =
-    acc &&
+  let rec aux ty =
     let ty = repr ty in
-    ty.level < lowest_level || (
-      ty.level = generic_level && (
-        mark_type_node ty;
-        fold_type_expr aux true ty
-      )
-    )
+    if not_marked_node ty then
+      if ty.level = generic_level then
+        (flip_mark_node ty; iter_type_expr aux ty)
+      else raise Exit
   in
-  let res = aux true ty in
+  let res = try aux ty; true with Exit -> false in
   unmark_type ty;
   res
 
@@ -1157,8 +1153,8 @@ let rec copy ?partial ?keep_names scope ty =
     For_copy.save_desc scope ty desc;
     let t = newvar() in          (* Stub *)
     set_scope t ty.scope;
-    ty.desc <- Tsubst t;
-    t.desc <-
+    Private_type_expr.set_desc ty (Tsubst t);
+    Private_type_expr.set_desc t
       begin match desc with
       | Tconstr (p, tl, _) ->
           let abbrevs = proper_abbrevs p tl !abbreviations in
@@ -1188,7 +1184,8 @@ let rec copy ?partial ?keep_names scope ty =
           begin match more.desc with
             Tsubst {desc = Ttuple [_;ty2]} ->
               (* This variant type has been already copied *)
-              ty.desc <- Tsubst ty2; (* avoid Tlink in the new type *)
+              Private_type_expr.set_desc ty (Tsubst ty2);
+              (* avoid Tlink in the new type *)
               Tlink ty2
           | _ ->
               (* If the row variable is not generic, we must keep it *)
@@ -1196,6 +1193,8 @@ let rec copy ?partial ?keep_names scope ty =
               let more' =
                 match more.desc with
                   Tsubst ty -> ty
+                  (* TODO: is this case possible?
+                     possibly an interaction with (copy more) below? *)
                 | Tconstr _ | Tnil ->
                     For_copy.save_desc scope more more.desc;
                     copy more
@@ -1235,7 +1234,8 @@ let rec copy ?partial ?keep_names scope ty =
                 | _ -> (more', row)
               in
               (* Register new type first for recursion *)
-              more.desc <- Tsubst(newgenty(Ttuple[more';t]));
+              Private_type_expr.set_desc
+                more (Tsubst(newgenty(Ttuple[more';t])));
               (* Return a new copy *)
               Tvariant (copy_row copy true row keep more')
           end
@@ -1435,7 +1435,7 @@ let rec copy_sep cleanup_scope fixed free bound visited ty =
     if ty.level <> generic_level then ty else
     let t = newvar () in
     delayed_copy :=
-      lazy (t.desc <- Tlink (copy cleanup_scope ty))
+      lazy (Private_type_expr.set_desc t (Tlink (copy cleanup_scope ty)))
       :: !delayed_copy;
     t
   else try
@@ -1453,7 +1453,7 @@ let rec copy_sep cleanup_scope fixed free bound visited ty =
           visited
     in
     let copy_rec = copy_sep cleanup_scope fixed free bound visited in
-    t.desc <-
+    Private_type_expr.set_desc t
       begin match ty.desc with
       | Tvariant row0 ->
           let row = row_repr row0 in
@@ -1910,6 +1910,7 @@ let local_non_recursive_abbrev env p ty =
 
 (* Since we cannot duplicate universal variables, unification must
    be done at meta-level, using bindings in univar_pairs *)
+(* TODO: use find_opt *)
 let rec unify_univar t1 t2 = function
     (cl1, cl2) :: rem ->
       let find_univ t cl =
@@ -1936,19 +1937,19 @@ let occur_univar env ty =
   let visited = ref TypeMap.empty in
   let rec occur_rec bound ty =
     let ty = repr ty in
-    if ty.level >= lowest_level &&
+    if not_marked_node ty then
       if TypeSet.is_empty bound then
-        (ty.level <- pivot_level - ty.level; true)
+        (flip_mark_node ty; occur_desc bound ty)
       else try
         let bound' = TypeMap.find ty !visited in
-        if TypeSet.exists (fun x -> not (TypeSet.mem x bound)) bound' then
-          (visited := TypeMap.add ty (TypeSet.inter bound bound') !visited;
-           true)
-        else false
+        if TypeSet.exists (fun x -> not (TypeSet.mem x bound)) bound' then begin
+          visited := TypeMap.add ty (TypeSet.inter bound bound') !visited;
+          occur_desc bound ty
+        end
       with Not_found ->
         visited := TypeMap.add ty bound !visited;
-        true
-    then
+        occur_desc bound ty
+  and occur_desc bound ty =
       match ty.desc with
         Tunivar _ ->
           if not (TypeSet.mem ty bound) then
@@ -2057,7 +2058,7 @@ let polyfy env ty vars =
     | Tvar name when ty.level = generic_level ->
         For_copy.save_desc scope ty ty.desc;
         let t = newty (Tunivar name) in
-        ty.desc <- Tsubst t;
+        Private_type_expr.set_desc ty (Tsubst t);
         Some t
     | _ -> None
   in
@@ -2105,9 +2106,8 @@ let expand_trace env trace =
 let deep_occur t0 ty =
   let rec occur_rec ty =
     let ty = repr ty in
-    if ty.level >= t0.level then begin
+    if ty.level >= t0.level && try_mark_node ty then begin
       if ty == t0 then raise Occur;
-      ty.level <- pivot_level - ty.level;
       iter_type_expr occur_rec ty
     end
   in
@@ -2115,30 +2115,6 @@ let deep_occur t0 ty =
     occur_rec ty; unmark_type ty; false
   with Occur ->
     unmark_type ty; true
-
-(*
-   1. When unifying two non-abbreviated types, one type is made a link
-      to the other. When unifying an abbreviated type with a
-      non-abbreviated type, the non-abbreviated type is made a link to
-      the other one. When unifying to abbreviated types, these two
-      types are kept distincts, but they are made to (temporally)
-      expand to the same type.
-   2. Abbreviations with at least one parameter are systematically
-      expanded. The overhead does not seem too high, and that way
-      abbreviations where some parameters does not appear in the
-      expansion, such as ['a t = int], are correctly handled. In
-      particular, for this example, unifying ['a t] with ['b t] keeps
-      ['a] and ['b] distincts. (Is it really important ?)
-   3. Unifying an abbreviation ['a t = 'a] with ['a] should not yield
-      ['a t as 'a]. Indeed, the type variable would otherwise be lost.
-      This problem occurs for abbreviations expanding to a type
-      variable, but also to many other constrained abbreviations (for
-      instance, [(< x : 'a > -> unit) t = <x : 'a>]). The solution is
-      that, if an abbreviation is unified with some subpart of its
-      parameters, then the parameter actually does not get
-      abbreviated.  It would be possible to check whether some
-      information is indeed lost, but it probably does not worth it.
-*)
 
 let gadt_equations_level = ref None
 
@@ -2448,9 +2424,9 @@ let find_lowest_level ty =
   let lowest = ref generic_level in
   let rec find ty =
     let ty = repr ty in
-    if ty.level >= lowest_level then begin
+    if not_marked_node ty then begin
       if ty.level < !lowest then lowest := ty.level;
-      ty.level <- pivot_level - ty.level;
+      flip_mark_node ty;
       iter_type_expr find ty
     end
   in find ty; unmark_type ty; !lowest
@@ -2580,7 +2556,7 @@ let unify1_var env t1 t2 =
     update_level env t1.level t2;
     update_scope t1.scope t2
   with Unify _ as e ->
-    t1.desc <- d1;
+    Private_type_expr.set_desc t1 d1;
     raise e
 
 (* Can only be called when generate_equations is true *)
@@ -2588,6 +2564,30 @@ let record_equation t1 t2 =
   match !equations_generation with
   | Forbidden -> assert false
   | Allowed { equated_types } -> TypePairs.add equated_types (t1, t2) ()
+
+(*
+   1. When unifying two non-abbreviated types, one type is made a link
+      to the other. When unifying an abbreviated type with a
+      non-abbreviated type, the non-abbreviated type is made a link to
+      the other one. When unifying to abbreviated types, these two
+      types are kept distincts, but they are made to (temporally)
+      expand to the same type.
+   2. Abbreviations with at least one parameter are systematically
+      expanded. The overhead does not seem too high, and that way
+      abbreviations where some parameters does not appear in the
+      expansion, such as ['a t = int], are correctly handled. In
+      particular, for this example, unifying ['a t] with ['b t] keeps
+      ['a] and ['b] distincts. (Is it really important ?)
+   3. Unifying an abbreviation ['a t = 'a] with ['a] should not yield
+      ['a t as 'a]. Indeed, the type variable would otherwise be lost.
+      This problem occurs for abbreviations expanding to a type
+      variable, but also to many other constrained abbreviations (for
+      instance, [(< x : 'a > -> unit) t = <x : 'a>]). The solution is
+      that, if an abbreviation is unified with some subpart of its
+      parameters, then the parameter actually does not get
+      abbreviated.  It would be possible to check whether some
+      information is indeed lost, but it probably does not worth it.
+*)
 
 let rec unify (env:Env.t ref) t1 t2 =
   (* First step: special cases (optimizations) *)
@@ -2844,7 +2844,7 @@ and unify3 env t1 t1' t2 t2' =
         | _ ->
             () (* t2 has already been expanded by update_level *)
     with Unify trace ->
-      t1'.desc <- d1;
+      Private_type_expr.set_desc t1' d1;
       raise (Unify trace)
   end
 
@@ -3110,7 +3110,6 @@ and unify_row_field env fixed1 fixed2 rm1 rm2 l f1 f2 =
       if_not_fixed second (fun () -> set_row_field e2 f1)
   | _ -> raise (Unify [])
 
-
 let unify env ty1 ty2 =
   let snap = Btype.snapshot () in
   try
@@ -3268,11 +3267,9 @@ let filter_self_method env lab priv meths ty =
 let moregen_occur env level ty =
   let rec occur ty =
     let ty = repr ty in
-    if ty.level > level then begin
-      if is_Tvar ty && ty.level >= generic_level - 1 then raise Occur;
-      ty.level <- pivot_level - ty.level;
-      iter_type_expr occur ty
-    end
+    if ty.level <= level then () else
+    if is_Tvar ty && ty.level >= generic_level - 1 then raise Occur else
+    if try_mark_node ty then iter_type_expr occur ty
   in
   begin try
     occur ty; unmark_type ty
@@ -3487,9 +3484,8 @@ let moregeneral env inst_nongen pat_sch subj_sch =
 
 let rec rigidify_rec vars ty =
   let ty = repr ty in
-  if ty.level >= lowest_level then begin
-    ty.level <- pivot_level - ty.level;
-    match ty.desc with
+  if try_mark_node ty then
+    begin match ty.desc with
     | Tvar _ ->
         if not (List.memq ty !vars) then vars := ty :: !vars
     | Tvariant row ->
@@ -3506,7 +3502,7 @@ let rec rigidify_rec vars ty =
         if not (static_row row) then rigidify_rec vars (row_more row)
     | _ ->
         iter_type_expr (rigidify_rec vars) ty
-  end
+    end
 
 let rigidify ty =
   let vars = ref [] in
@@ -4116,7 +4112,7 @@ let rec build_subtype env visited loops posi level t =
              as this occurrence might break the occur check.
              XXX not clear whether this correct anyway... *)
           if List.exists (deep_occur ty) tl1 then raise Not_found;
-          ty.desc <- Tvar None;
+          set_type_desc ty (Tvar None);
           let t'' = newvar () in
           let loops = (ty, t'') :: loops in
           (* May discard [visited] as level is going down *)
@@ -4125,7 +4121,7 @@ let rec build_subtype env visited loops posi level t =
           assert (is_Tvar t'');
           let nm =
             if c > Equiv || deep_occur ty ty1' then None else Some(p,tl1) in
-          t''.desc <- Tobject (ty1', ref nm);
+          set_type_desc t'' (Tobject (ty1', ref nm));
           (try unify_var env ty t with Unify _ -> assert false);
           (t'', Changed)
       | _ -> raise Not_found
@@ -4641,7 +4637,7 @@ let rec nondep_type_rec ?(expand_private=false) env ids ty =
   with Not_found ->
     let ty' = newgenvar () in        (* Stub *)
     TypeHash.add nondep_hash ty ty';
-    ty'.desc <-
+    set_type_desc ty'
       begin match ty.desc with
       | Tconstr(p, tl, _abbrev) ->
           begin try

typing/btype.ml
@@ -45,7 +45,8 @@ let pivot_level = 2 * lowest_level - 1
 let new_id = s_ref (-1)
 
 let newty2 level desc  =
-  incr new_id; { desc; level; scope = lowest_level; id = !new_id }
+  incr new_id;
+  Private_type_expr.create desc ~level ~scope:lowest_level ~id:!new_id
 let newgenty desc      = newty2 generic_level desc
 let newgenvar ?name () = newgenty (Tvar name)
 (*
@@ -77,7 +78,6 @@ type change =
   | Ckind of field_kind option ref * field_kind option
   | Ccommu of commutable ref * commutable
   | Cuniv of type_expr option ref * type_expr option
-  | Ctypeset of TypeSet.t ref * TypeSet.t
 
 type changes =
     Change of change * changes ref
@@ -100,7 +100,7 @@ let rec field_kind_repr =
     Fvar {contents = Some kind} -> field_kind_repr kind
   | kind                        -> kind
 
-let rec repr_link compress t d =
+let rec repr_link compress (t : type_expr) d : type_expr -> type_expr =
  function
    {desc = Tlink t' as d'} ->
      repr_link true t d' t'
@@ -108,11 +108,11 @@ let rec repr_link compress t d =
      repr_link true t d' t'
  | t' ->
      if compress then begin
-       log_change (Ccompress (t, t.desc, d)); t.desc <- d
+       log_change (Ccompress (t, t.desc, d)); Private_type_expr.set_desc t d
      end;
      t'
 
-let repr t =
+let repr (t : type_expr) =
   match t.desc with
    Tlink t' as d ->
      repr_link false t d t'
@@ -255,6 +255,20 @@ let is_constr_row ~allow_ident t =
       is_row_name (Ident.name id)
   | Tconstr (Path.Pdot (_, s), _, _) -> is_row_name s
   | _ -> false
+
+(* TODO: where should this really be *)
+(* Set row_name in Env, cf. GPR#1204/1329 *)
+let set_row_name decl path =
+  match decl.type_manifest with
+    None -> ()
+  | Some ty ->
+      let ty = repr ty in
+      match ty.desc with
+        Tvariant row when static_row row ->
+          let row = {(row_repr row) with
+                     row_name = Some (path, decl.type_params)} in
+          Private_type_expr.set_desc ty (Tvariant row)
+      | _ -> ()
 
 
                   (**********************************)
@@ -538,7 +552,7 @@ end = struct
 
   (* Restore type descriptions. *)
   let cleanup { saved_desc; saved_kinds; _ } =
-    List.iter (fun (ty, desc) -> ty.desc <- desc) saved_desc;
+    List.iter (fun (ty, desc) -> Private_type_expr.set_desc ty desc) saved_desc;
     List.iter (fun r -> r := None) saved_kinds
 
   let with_scope f =
@@ -549,17 +563,19 @@ end = struct
 end
 
 (* Mark a type. *)
+
+let not_marked_node ty = ty.level >= lowest_level
+    (* type nodes with negative levels are "marked" *)
+
+let flip_mark_node ty = Private_type_expr.set_level ty (pivot_level - ty.level)
+
+let try_mark_node ty = not_marked_node ty && (flip_mark_node ty; true)
+
 let rec mark_type ty =
   let ty = repr ty in
-  if ty.level >= lowest_level then begin
-    ty.level <- pivot_level - ty.level;
+  if not_marked_node ty then begin
+    flip_mark_node ty;
     iter_type_expr mark_type ty
-  end
-
-let mark_type_node ty =
-  let ty = repr ty in
-  if ty.level >= lowest_level then begin
-    ty.level <- pivot_level - ty.level;
   end
 
 let mark_type_params ty =
@@ -568,10 +584,7 @@ let mark_type_params ty =
 let type_iterators =
   let it_type_expr it ty =
     let ty = repr ty in
-    if ty.level >= lowest_level then begin
-      mark_type_node ty;
-      it.it_do_type_expr it ty;
-    end
+    if try_mark_node ty then it.it_do_type_expr it ty
   in
   {type_iterators with it_type_expr}
 
@@ -580,7 +593,8 @@ let type_iterators =
 let rec unmark_type ty =
   let ty = repr ty in
   if ty.level < lowest_level then begin
-    ty.level <- pivot_level - ty.level;
+    (* flip back the marked level *)
+    flip_mark_node ty;
     iter_type_expr unmark_type ty
   end
 
@@ -708,16 +722,15 @@ let extract_label l ls = extract_label_aux [] l ls
                   (**********************************)
 
 let undo_change = function
-    Ctype  (ty, desc) -> ty.desc <- desc
-  | Ccompress  (ty, desc, _) -> ty.desc <- desc
-  | Clevel (ty, level) -> ty.level <- level
-  | Cscope (ty, scope) -> ty.scope <- scope
+    Ctype  (ty, desc) -> Private_type_expr.set_desc ty desc
+  | Ccompress  (ty, desc, _) -> Private_type_expr.set_desc ty desc
+  | Clevel (ty, level) -> Private_type_expr.set_level ty level
+  | Cscope (ty, scope) -> Private_type_expr.set_scope ty scope
   | Cname  (r, v) -> r := v
   | Crow   (r, v) -> r := v
   | Ckind  (r, v) -> r := v
   | Ccommu (r, v) -> r := v
   | Cuniv  (r, v) -> r := v
-  | Ctypeset (r, v) -> r := v
 
 type snapshot = changes ref * int
 let last_snapshot = s_ref 0
@@ -727,35 +740,40 @@ let log_type ty =
 let link_type ty ty' =
   log_type ty;
   let desc = ty.desc in
-  ty.desc <- Tlink ty';
+  Private_type_expr.set_desc ty (Tlink ty');
   (* Name is a user-supplied name for this unification variable (obtained
    * through a type annotation for instance). *)
   match desc, ty'.desc with
     Tvar name, Tvar name' ->
       begin match name, name' with
-      | Some _, None ->  log_type ty'; ty'.desc <- Tvar name
-      | None, Some _ ->  ()
+      | Some _, None -> log_type ty'; Private_type_expr.set_desc ty' (Tvar name)
+      | None, Some _ -> ()
       | Some _, Some _ ->
-          if ty.level < ty'.level then (log_type ty'; ty'.desc <- Tvar name)
-      | None, None   ->  ()
+          if ty.level < ty'.level then
+            (log_type ty'; Private_type_expr.set_desc ty' (Tvar name))
+      | None, None   -> ()
       end
   | _ -> ()
   (* ; assert (check_memorized_abbrevs ()) *)
   (*  ; check_expans [] ty' *)
+(* TODO: consider eliminating set_type_desc, replacing it with link types *)
 let set_type_desc ty td =
   if td != ty.desc then begin
     log_type ty;
-    ty.desc <- td
+    Private_type_expr.set_desc ty td
   end
-let set_level ty level =
+(* TODO: separate set_level into two specific functions: *)
+(*  set_lower_level and set_generic_level *)
+ let set_level ty level =
   if level <> ty.level then begin
     if ty.id <= !last_snapshot then log_change (Clevel (ty, ty.level));
-    ty.level <- level
+    Private_type_expr.set_level ty level
   end
+(* TODO: introduce a guard and rename it to set_higher_scope? *)
 let set_scope ty scope =
   if scope <> ty.scope then begin
     if ty.id <= !last_snapshot then log_change (Cscope (ty, ty.scope));
-    ty.scope <- scope
+    Private_type_expr.set_scope ty scope
   end
 let set_univar rty ty =
   log_change (Cuniv (rty, !rty)); rty := Some ty
@@ -767,8 +785,6 @@ let set_kind rk k =
   log_change (Ckind (rk, !rk)); rk := Some k
 let set_commu rc c =
   log_change (Ccommu (rc, !rc)); rc := c
-let set_typeset rs s =
-  log_change (Ctypeset (rs, !rs)); rs := s
 
 let snapshot () =
   let old = !last_snapshot in
@@ -817,6 +833,6 @@ let undo_compress (changes, _old) =
       List.iter
         (fun r -> match !r with
           Change (Ccompress (ty, desc, d), next) when ty.desc == d ->
-            ty.desc <- desc; r := !next
+            Private_type_expr.set_desc ty desc; r := !next
         | _ -> ())
         log

typing/btype.mli
@@ -100,6 +100,9 @@ val has_constr_row: type_expr -> bool
 val is_row_name: string -> bool
 val is_constr_row: allow_ident:bool -> type_expr -> bool
 
+(* Set the polymorphic variant row_name field *)
+val set_row_name : type_declaration -> Path.t -> unit
+
 (**** Utilities for type traversal ****)
 
 val iter_type_expr: (type_expr -> unit) -> type_expr -> unit
@@ -130,7 +133,7 @@ type type_iterators =
     it_path: Path.t -> unit; }
 val type_iterators: type_iterators
         (* Iteration on arbitrary type information.
-           [it_type_expr] calls [mark_type_node] to avoid loops. *)
+           [it_type_expr] calls [mark_node] to avoid loops. *)
 val unmark_iterators: type_iterators
         (* Unmark any structure containing types. See [unmark_type] below. *)
 
@@ -164,14 +167,17 @@ end
 
 val lowest_level: int
         (* Marked type: ty.level < lowest_level *)
-val pivot_level: int
-        (* Type marking: ty.level <- pivot_level - ty.level *)
+val not_marked_node: type_expr -> bool
+        (* Return true if a type node is not yet marked *)
+val flip_mark_node: type_expr -> unit
+        (* Mark a type node. No [repr]'ing *)
+val try_mark_node: type_expr -> bool
+        (* Mark a type node if it is not yet marked.
+           Return false if it was already marked *)
 val mark_type: type_expr -> unit
-        (* Mark a type *)
-val mark_type_node: type_expr -> unit
-        (* Mark a type node (but not its sons) *)
+        (* Mark a type recursively *)
 val mark_type_params: type_expr -> unit
-        (* Mark the sons of a type node *)
+        (* Mark the sons of a type node recursively *)
 val unmark_type: type_expr -> unit
 val unmark_type_decl: type_declaration -> unit
 val unmark_extension_constructor: extension_constructor -> unit
@@ -241,7 +247,6 @@ val set_row_field: row_field option ref -> row_field -> unit
 val set_univar: type_expr option ref -> type_expr -> unit
 val set_kind: field_kind option ref -> field_kind -> unit
 val set_commu: commutable ref -> commutable -> unit
-val set_typeset: TypeSet.t ref -> TypeSet.t -> unit
         (* Set references, logging the old value *)
 
 (**** Forward declarations ****)

typing/datarepr.ml
@@ -25,8 +25,7 @@ let free_vars ?(param=false) ty =
   let ret = ref TypeSet.empty in
   let rec loop ty =
     let ty = repr ty in
-    if ty.level >= lowest_level then begin
-      ty.level <- pivot_level - ty.level;
+    if try_mark_node ty then
       match ty.desc with
       | Tvar _ ->
           ret := TypeSet.add ty !ret
@@ -41,7 +40,6 @@ let free_vars ?(param=false) ty =
       (* XXX: What about Tobject ? *)
       | _ ->
           iter_type_expr loop ty
-    end
   in
   loop ty;
   unmark_type ty;
@@ -182,7 +180,8 @@ let extension_descr ~current_unit path_ext ext =
       cstr_uid = ext.ext_uid;
     }
 
-let none = {desc = Ttuple []; level = -1; scope = Btype.generic_level; id = -1}
+let none = Private_type_expr.create (Ttuple [])
+    ~level:(-1) ~scope:Btype.generic_level ~id:(-1)
                                         (* Clearly ill-formed type *)
 let dummy_label =
   { lbl_name = ""; lbl_res = none; lbl_arg = none; lbl_mut = Immutable;
@@ -243,16 +242,3 @@ let labels_of_type ty_path decl =
       label_descrs (newgenconstr ty_path decl.type_params)
         labels rep decl.type_private
   | Type_variant _ | Type_abstract | Type_open -> []
-
-(* Set row_name in Env, cf. GPR#1204/1329 *)
-let set_row_name decl path =
-  match decl.type_manifest with
-    None -> ()
-  | Some ty ->
-      let ty = repr ty in
-      match ty.desc with
-        Tvariant row when static_row row ->
-          let row = {(row_repr row) with
-                     row_name = Some (path, decl.type_params)} in
-          ty.desc <- Tvariant row
-      | _ -> ()

typing/datarepr.mli
@@ -43,7 +43,3 @@ val constructor_existentials :
     - the types of the constructor's arguments
     - the existential variables introduced by the constructor
  *)
-
-
-(* Set the polymorphic variant row_name field *)
-val set_row_name : type_declaration -> Path.t -> unit

typing/env.ml
@@ -1551,7 +1551,7 @@ let rec components_of_module_maker
               may_subst Subst.type_declaration freshening_sub decl
             in
             let final_decl = Subst.type_declaration prefixing_sub fresh_decl in
-            Datarepr.set_row_name final_decl
+            Btype.set_row_name final_decl
               (Subst.type_path prefixing_sub (Path.Pident id));
             let constructors =
               List.map snd

typing/printtyp.ml
@@ -1182,7 +1182,7 @@ let rec tree_of_type_decl id decl =
       let vars = free_variables ty in
       List.iter
         (function {desc = Tvar (Some "_")} as ty ->
-            if List.memq ty vars then ty.desc <- Tvar None
+            if List.memq ty vars then set_type_desc ty (Tvar None)
           | _ -> ())
         params
   | None -> ()

typing/subst.ml
@@ -131,7 +131,8 @@ let reset_for_saving () = new_id := -1
 
 let newpersty desc =
   decr new_id;
-  { desc; level = generic_level; scope = Btype.lowest_level; id = !new_id }
+  Private_type_expr.create
+    desc ~level:generic_level ~scope:Btype.lowest_level ~id:!new_id
 
 (* ensure that all occurrences of 'Tvar None' are physically shared *)
 let tvar_none = Tvar None
@@ -154,7 +155,9 @@ let rec typexp copy_scope s ty =
           else newty2 ty.level desc
         in
         For_copy.save_desc copy_scope ty desc;
-        ty.desc <- Tsubst ty';
+        Private_type_expr.set_desc ty (Tsubst ty');
+        (* TODO: move this line to btype.ml
+           there is a similar problem also in ctype.ml *)
         ty'
       else ty
   | Tsubst ty ->
@@ -175,9 +178,10 @@ let rec typexp copy_scope s ty =
       not (is_Tconstr ty) && is_constr_row ~allow_ident:false tm in
     (* Make a stub *)
     let ty' = if s.for_saving then newpersty (Tvar None) else newgenvar () in
-    ty'.scope <- ty.scope;
-    ty.desc <- Tsubst ty';
-    ty'.desc <-
+    Private_type_expr.set_scope ty' ty.scope;
+    (* TODO: figure out why not use set_scope *)
+    Private_type_expr.set_desc ty (Tsubst ty');
+    Private_type_expr.set_desc ty'
       begin if has_fixed_row then
         match tm.desc with (* PR#7348 *)
           Tconstr (Pdot(m,i), tl, _abbrev) ->
@@ -214,7 +218,8 @@ let rec typexp copy_scope s ty =
           begin match more.desc with
             Tsubst {desc = Ttuple [_;ty2]} ->
               (* This variant type has been already copied *)
-              ty.desc <- Tsubst ty2; (* avoid Tlink in the new type *)
+              Private_type_expr.set_desc ty (Tsubst ty2);
+              (* avoid Tlink in the new type *)
               Tlink ty2
           | _ ->
               let dup =
@@ -232,7 +237,9 @@ let rec typexp copy_scope s ty =
                 | _ -> assert false
               in
               (* Register new type first for recursion *)
-              more.desc <- Tsubst(newgenty(Ttuple[more';ty']));
+              Private_type_expr.set_desc more
+                (Tsubst(newgenty(Ttuple[more';ty'])));
+              (* TODO: check if more' can be eliminated *)
               (* Return a new copy *)
               let row =
                 copy_row (typexp copy_scope s) true row (not dup) more' in

commit 012e80ee2993064b76bc3ccbb6d97ed2aed6b7ae

generalize: remove incorrect comment
Introduced in #9994

typing/ctype.ml
@@ -727,7 +727,6 @@ let duplicate_class_type ty =
 *)
 let rec generalize ty =
   let ty = repr ty in
-  (* generalize the type iff ty.level <= !current_level *)
   if (ty.level > !current_level) && (ty.level <> generic_level) then begin
     set_level ty generic_level;
     (* recur into abbrev for the speed *)