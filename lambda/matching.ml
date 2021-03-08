ostera/bs-ocaml

no weird name temporarily - avoid huge diffs

bytecomp/matching.ml
@@ -2704,7 +2704,7 @@ let rec name_pattern default = function
 let arg_to_var arg cls = match arg with
 | Lvar v -> v,arg
 | _ ->
-    let v = name_pattern "*match*" cls in
+    let v = name_pattern "match" cls in
     v,Lvar v
 
 
@@ -3197,7 +3197,7 @@ let do_for_multiple_match loc paraml pat_act_list partial =
       let next, nexts = split_precompile None pm1 in
 
       let size = List.length paraml
-      and idl = List.map (fun _ -> Ident.create "*match*") paraml in
+      and idl = List.map (fun _ -> Ident.create "match") paraml in
       let args =  List.map (fun id -> Lvar id, Alias) idl in
 
       let flat_next = flatten_precompiled size args next
@@ -3234,7 +3234,7 @@ let do_for_multiple_match loc paraml pat_act_list partial =
 
 let param_to_var param = match param with
 | Lvar v -> v,None
-| _ -> Ident.create "*match*",Some param
+| _ -> Ident.create "match",Some param
 
 let bind_opt (v,eo) k = match eo with
 | None -> k

melange-re/melange-compiler-libs

lambda/matching.ml
@@ -3108,7 +3108,7 @@ let arg_to_var arg cls =
   match arg with
   | Lvar v -> (v, arg)
   | _ ->
-      let v = name_pattern "*match*" cls in
+      let v = name_pattern "match" cls in
       (v, Lvar v)
 
 (*
@@ -3750,7 +3750,7 @@ let do_for_multiple_match ~scopes loc paraml pat_act_list partial =
         end
     | next, nexts ->
         let size = List.length paraml
-        and idl = List.map (fun _ -> Ident.create_local "*match*") paraml in
+        and idl = List.map (fun _ -> Ident.create_local "match") paraml in
         let args = List.map (fun id -> (Lvar id, Alias)) idl in
         let flat_next = flatten_precompiled size args next
         and flat_nexts =
@@ -3779,7 +3779,7 @@ let do_for_multiple_match ~scopes loc paraml pat_act_list partial =
 let param_to_var param =
   match param with
   | Lvar v -> (v, None)
-  | _ -> (Ident.create_local "*match*", Some param)
+  | _ -> (Ident.create_local "match", Some param)
 
 let bind_opt (v, eo) k =
   match eo with