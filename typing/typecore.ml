BuckleScript/ocaml

add warning 109 to signal  top-level expressions have type unit

typing/typecore.ml
@@ -4873,6 +4873,11 @@ let type_expression env sexp =
   Typetexp.reset_type_variables();
   begin_def();
   let exp = type_exp env sexp in
+  if Warnings.is_active Bs_toplevel_expression_unit then 
+    (try unify env exp.exp_type 
+      (instance_def Predef.type_unit) with 
+    | Unify _ 
+    | Tags _  -> Location.prerr_warning sexp.pexp_loc Bs_toplevel_expression_unit);
   end_def();
   if not (is_nonexpansive exp) then generalize_expansive env exp.exp_type;
   generalize exp.exp_type;

melange-re/melange-compiler-libs

typing/typecore.ml
@@ -5120,6 +5120,11 @@ let type_expression env sexp =
   Typetexp.reset_type_variables();
   begin_def();
   let exp = type_exp env sexp in
+  if Warnings.is_active Bs_toplevel_expression_unit then
+    (try unify env exp.exp_type
+      (instance Predef.type_unit) with
+    | Unify _
+    | Tags _  -> Location.prerr_warning sexp.pexp_loc Bs_toplevel_expression_unit);
   end_def();
   if maybe_expansive exp then lower_contravariant env exp.exp_type;
   generalize exp.exp_type;