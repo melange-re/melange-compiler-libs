jchavarri/ocaml

[compiler] change Field_name Ident.t to Field_name string to make error reporting more useful

typing/includecore.ml
@@ -140,7 +140,7 @@ type type_mismatch =
   | Field_type of Ident.t
   | Field_mutable of Ident.t
   | Field_arity of Ident.t
-  | Field_names of int * Ident.t * Ident.t
+  | Field_names of int * string * string
   | Field_missing of bool * Ident.t
   | Record_representation of bool   (* true means second one is unboxed float *)
   | Unboxed_representation of bool  (* true means second one is unboxed *)
@@ -163,7 +163,7 @@ let report_type_mismatch0 first second decl ppf err =
       pr "The arities for field %s differ" (Ident.name s)
   | Field_names (n, name1, name2) ->
       pr "Fields number %i have different names, %s and %s"
-        n (Ident.name name1) (Ident.name name2)
+        n name1 name2
   | Field_missing (b, s) ->
       pr "The field %s is only present in %s %s"
         (Ident.name s) (if b then second else first) decl
@@ -204,7 +204,7 @@ and compare_variants ~loc env params1 params2 n
   | c::_, [] -> [Field_missing (false, c.Types.cd_id)]
   | cd1::rem1, cd2::rem2 ->
       if Ident.name cd1.cd_id <> Ident.name cd2.cd_id then
-        [Field_names (n, cd1.cd_id, cd2.cd_id)]
+        [Field_names (n, cd1.cd_id.name, cd2.cd_id.name)]
       else begin
         Builtin_attributes.check_deprecated_inclusion
           ~def:cd1.cd_loc
@@ -239,7 +239,7 @@ and compare_records ~loc env params1 params2 n
   | l::_, [] -> [Field_missing (false, l.Types.ld_id)]
   | ld1::rem1, ld2::rem2 ->
       if Ident.name ld1.ld_id <> Ident.name ld2.ld_id
-      then [Field_names (n, ld1.ld_id, ld2.ld_id)]
+      then [Field_names (n, ld1.ld_id.name, ld2.ld_id.name)]
       else if ld1.ld_mutable <> ld2.ld_mutable then [Field_mutable ld1.ld_id] else begin
         Builtin_attributes.check_deprecated_mutable_inclusion
           ~def:ld1.ld_loc

typing/includecore.mli
@@ -30,7 +30,7 @@ type type_mismatch =
   | Field_type of Ident.t
   | Field_mutable of Ident.t
   | Field_arity of Ident.t
-  | Field_names of int * Ident.t * Ident.t
+  | Field_names of int * string * string
   | Field_missing of bool * Ident.t
   | Record_representation of bool
   | Unboxed_representation of bool

melange-re/melange-compiler-libs

typing/includecore.ml
@@ -34,9 +34,9 @@ let value_descriptions ~loc env name
     vd1.val_attributes vd2.val_attributes
 #if undefined BS_NO_COMPILER_PATCH then
     (Ident.name name);
-#else    
+#else
     name;
-#end  
+#end
   if Ctype.moregeneral env true vd1.val_type vd2.val_type then begin
     match (vd1.val_kind, vd2.val_kind) with
         (Val_prim p1, Val_prim p2) ->
@@ -150,7 +150,7 @@ type record_mismatch =
   | Label_mismatch of Types.label_declaration
                       * Types.label_declaration
                       * label_mismatch
-  | Label_names of int * Ident.t * Ident.t
+  | Label_names of int * string * string
   | Label_missing of position * Ident.t
   | Unboxed_float_representation of position
 
@@ -208,7 +208,7 @@ let report_record_mismatch first second decl ppf err =
         (report_label_mismatch first second) err
   | Label_names (n, name1, name2) ->
       pr "@[<hv>Fields number %i have different names, %s and %s.@]"
-        n (Ident.name name1) (Ident.name name2)
+        n name1 name2
   | Label_missing (ord, s) ->
       pr "@[<hv>The field %s is only present in %s %s.@]"
         (Ident.name s) (choose ord first second) decl
@@ -360,7 +360,7 @@ and compare_records ~loc env params1 params2 n
   | l::_, [] -> Some (Label_missing (First, l.Types.ld_id))
   | ld1::rem1, ld2::rem2 ->
       if Ident.name ld1.ld_id <> Ident.name ld2.ld_id
-      then Some (Label_names (n, ld1.ld_id, ld2.ld_id))
+      then Some (Label_names (n, Ident.name ld1.ld_id, Ident.name ld2.ld_id))
       else begin
         Builtin_attributes.check_deprecated_mutable_inclusion
           ~def:ld1.ld_loc

typing/includecore.mli
@@ -28,7 +28,7 @@ type label_mismatch =
 
 type record_mismatch =
   | Label_mismatch of label_declaration * label_declaration * label_mismatch
-  | Label_names of int * Ident.t * Ident.t
+  | Label_names of int * string * string
   | Label_missing of position * Ident.t
   | Unboxed_float_representation of position
 