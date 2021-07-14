trefis/ocaml

Treat class_signature more like type_expr
Treat class_signature more like type_expr by making all its
components mutable and using three unification-like operations
for manipulating them: add_method, add_instance_variable and
inherit_class_signature. These operations behave similarly to
filter_self_method, which they replace.

We move much of the logic for handling class_signature into Btype
and Ctype and use it for the typing of class signatures
and class structures.

Instead of using method and variable tables with both identifiers and
type components as accumulators during class structure typing, we use
tables with just identifiers along with a single class_signature. This
makes the logic clearer and makes it easier to share things with the
typing of class signatures.

typing/ctype.mli
@@ -186,11 +186,17 @@ val generalize_structure: type_expr -> unit
            to !current_level *)
 val generalize_spine: type_expr -> unit
         (* Special function to generalize a method during inference *)
+val generalize_class_type : class_type -> unit
+        (* Generalize the components of a class type *)
+val generalize_class_type_structure : class_type -> unit
+       (* Generalize the structure of the components of a class type *)
 val correct_levels: type_expr -> type_expr
         (* Returns a copy with decreasing levels *)
 val limited_generalize: type_expr -> type_expr -> unit
         (* Only generalize some part of the type
            Make the remaining of the type non-generalizable *)
+val limited_generalize_class_type: type_expr -> class_type -> unit
+        (* Same, but for class types *)
 
 val fully_generic: type_expr -> bool
 
@@ -227,6 +233,7 @@ val generic_instance_declaration: type_declaration -> type_declaration
         (* Same as instance_declaration, but new nodes at generic_level *)
 val instance_class:
         type_expr list -> class_type -> type_expr list * class_type
+
 val instance_poly:
         ?keep_names:bool ->
         bool -> type_expr list -> type_expr -> type_expr list * type_expr
@@ -276,13 +283,6 @@ val check_filter_method: Env.t -> string -> private_flag -> type_expr -> unit
         (* A special case of unification (with {m : 'a; 'b}), returning unit. *)
 val occur_in: Env.t -> type_expr -> type_expr -> bool
 val deep_occur: type_expr -> type_expr -> bool
-val filter_self_method:
-        Env.t -> string -> private_flag -> virtual_flag ->
-        (Ident.t * private_flag * virtual_flag * type_expr) Meths.t ->
-        type_expr ->
-        bool
-        * (Ident.t * private_flag * virtual_flag * type_expr) Meths.t
-        * (Ident.t * private_flag * virtual_flag * type_expr)
 val moregeneral: Env.t -> bool -> type_expr -> type_expr -> bool
         (* Check if the first type scheme is more general than the second. *)
 
@@ -334,6 +334,42 @@ val subtype: Env.t -> type_expr -> type_expr -> unit -> unit
            enforce and returns a function that enforces this
            constraints. *)
 
+(* Operations on class signatures *)
+
+val new_class_signature : unit -> class_signature
+val add_dummy_method : Env.t -> scope:int -> class_signature -> unit
+
+type add_method_failure =
+  | Unexpected_method
+  | Type_mismatch of Unification_trace.t
+
+exception Add_method_failed of add_method_failure
+
+val add_method : Env.t ->
+  label -> private_flag -> virtual_flag -> type_expr -> class_signature -> unit
+
+type add_instance_variable_failure =
+  | Mutability_mismatch of mutable_flag
+  | Type_mismatch of Unification_trace.t
+
+exception Add_instance_variable_failed of add_instance_variable_failure
+
+val add_instance_variable : strict:bool -> Env.t ->
+  label -> mutable_flag -> virtual_flag -> type_expr -> class_signature -> unit
+
+type inherit_class_signature_failure =
+  | Self_type_mismatch of Unification_trace.t
+  | Method of label * add_method_failure
+  | Instance_variable of label * add_instance_variable_failure
+
+exception Inherit_class_signature_failed of inherit_class_signature_failure
+
+val inherit_class_signature : strict:bool -> Env.t ->
+  class_signature -> class_signature -> unit
+
+val complete_class_signature :
+  Env.t -> class_signature -> label list * label list
+
 exception Nondep_cannot_erase of Ident.t
 
 val nondep_type: Env.t -> Ident.t list -> type_expr -> type_expr
@@ -359,9 +395,13 @@ val cyclic_abbrev: Env.t -> Ident.t -> type_expr -> bool
 val is_contractive: Env.t -> Path.t -> bool
 val normalize_type: type_expr -> unit
 
-val closed_schema: Env.t -> type_expr -> bool
+val nongen_schema: Env.t -> type_expr -> bool
         (* Check whether the given type scheme contains no non-generic
            type variables *)
+
+val nongen_class_declaration: class_declaration -> bool
+        (* Check whether the given class type contains no non-generic
+           type variables. Uses the empty environment.  *)
 
 val free_variables: ?env:Env.t -> type_expr -> type_expr list
         (* If env present, then check for incomplete definitions too *)
@@ -373,10 +413,7 @@ val closed_class:
         (* Check whether all type variables are bound *)
 
 val unalias: type_expr -> type_expr
-val signature_of_class_type: class_type -> class_signature
-val self_type: class_type -> type_expr
-val self_type_row: class_type -> type_expr
-val class_type_arity: class_type -> int
+
 val arity: type_expr -> int
         (* Return the arity (as for curried functions) of the given type. *)
 

typing/printtyp.ml
@@ -1450,7 +1450,7 @@ let tree_of_method sch (lab, priv, virt, ty) =
 
 let rec prepare_class_type params = function
   | Cty_constr (_p, tyl, cty) ->
-      let sty = Ctype.self_type_row cty in
+      let sty = Btype.self_type_row cty in
       if List.memq (proxy sty) !visited_objects
       || not (List.for_all is_Tvar params)
       || List.exists (deep_occur sty) tyl
@@ -1471,7 +1471,7 @@ let rec prepare_class_type params = function
 let rec tree_of_class_type sch params =
   function
   | Cty_constr (p', tyl, cty) ->
-      let sty = Ctype.self_type_row cty in
+      let sty = Btype.self_type_row cty in
       if List.memq (proxy sty) !visited_objects
       || not (List.for_all is_Tvar params)
       then
@@ -1552,7 +1552,7 @@ let tree_of_class_declaration id cl rs =
   reset_except_context ();
   List.iter add_alias params;
   prepare_class_type params cl.cty_type;
-  let row = Ctype.self_type_row cl.cty_type in
+  let row = Btype.self_type_row cl.cty_type in
   List.iter mark_loops params;
 
   List.iter check_name_of_type (List.map proxy params);
@@ -1574,13 +1574,13 @@ let tree_of_cltype_declaration id cl rs =
   reset_except_context ();
   List.iter add_alias params;
   prepare_class_type params cl.clty_type;
-  let row = Ctype.self_type_row cl.clty_type in
+  let row = Btype.self_type_row cl.clty_type in
   List.iter mark_loops params;
 
   List.iter check_name_of_type (List.map proxy params);
   if is_aliased row then check_name_of_type (proxy row);
 
-  let sign = Ctype.signature_of_class_type cl.clty_type in
+  let sign = Btype.signature_of_class_type cl.clty_type in
   let has_virtual_vars =
     Vars.fold (fun _ (_,vr,_) b -> vr = Virtual || b)
       sign.csig_vars false

melange-re/melange-compiler-libs

typing/ctype.mli
@@ -100,7 +100,6 @@ val close_object: type_expr -> bool
 val set_object_name:
         Ident.t -> type_expr list -> type_expr -> unit
 val remove_object_name: type_expr -> unit
-val hide_private_methods: type_expr -> unit
 val find_cltype_for_path: Env.t -> Path.t -> type_declaration * type_expr
 
 val sort_row_fields: (label * row_field) list -> (label * row_field) list
@@ -119,9 +118,10 @@ val lower_contravariant: Env.t -> type_expr -> unit
 val generalize_structure: type_expr -> unit
         (* Generalize the structure of a type, lowering variables
            to !current_level *)
-val generalize_class_type :
-  bool -> class_type -> unit
+val generalize_class_type : class_type -> unit
         (* Generalize the components of a class type *)
+val generalize_class_type_structure : class_type -> unit
+       (* Generalize the structure of the components of a class type *)
 val generalize_spine: type_expr -> unit
         (* Special function to generalize a method during inference *)
 val correct_levels: type_expr -> type_expr
@@ -129,6 +129,8 @@ val correct_levels: type_expr -> type_expr
 val limited_generalize: type_expr -> type_expr -> unit
         (* Only generalize some part of the type
            Make the remaining of the type non-generalizable *)
+val limited_generalize_class_type: type_expr -> class_type -> unit
+        (* Same, but for class types *)
 
 val fully_generic: type_expr -> bool
 
@@ -165,6 +167,7 @@ val generic_instance_declaration: type_declaration -> type_declaration
         (* Same as instance_declaration, but new nodes at generic_level *)
 val instance_class:
         type_expr list -> class_type -> type_expr list * class_type
+
 val instance_poly:
         ?keep_names:bool ->
         bool -> type_expr list -> type_expr -> type_expr list * type_expr
@@ -233,20 +236,8 @@ val filter_arrow: Env.t -> type_expr -> arg_label -> type_expr * type_expr
 val filter_method: Env.t -> string -> private_flag -> type_expr -> type_expr
         (* A special case of unification (with {m : 'a; 'b}).  Raises
            [Filter_method_failed] instead of [Unify]. *)
-val check_filter_method: Env.t -> string -> private_flag -> type_expr -> unit
-        (* A special case of unification (with {m : 'a; 'b}), returning unit.
-           Raises [Filter_method_failed] instead of [Unify]. *)
 val occur_in: Env.t -> type_expr -> type_expr -> bool
 val deep_occur: type_expr -> type_expr -> bool
-val filter_self_method:
-        Env.t -> string -> private_flag -> virtual_flag ->
-        (Ident.t * private_flag * virtual_flag * type_expr) Meths.t ->
-        type_expr ->
-        bool
-        * (Ident.t * private_flag * virtual_flag * type_expr) Meths.t
-        * (Ident.t * private_flag * virtual_flag * type_expr)
-        (* Raises [Filter_method_failed] instead of [Unify], and only if the
-           self type is closed at this point. *)
 val moregeneral: Env.t -> bool -> type_expr -> type_expr -> unit
         (* Check if the first type scheme is more general than the second. *)
 val is_moregeneral: Env.t -> bool -> type_expr -> type_expr -> bool
@@ -331,6 +322,44 @@ val subtype: Env.t -> type_expr -> type_expr -> unit -> unit
            enforce and returns a function that enforces this
            constraints. *)
 
+(* Operations on class signatures *)
+
+val new_class_signature : unit -> class_signature
+val add_dummy_method : Env.t -> scope:int -> class_signature -> unit
+
+type add_method_failure =
+  | Unexpected_method
+  | Type_mismatch of Errortrace.unification_error
+
+exception Add_method_failed of add_method_failure
+
+val add_method : Env.t ->
+  label -> private_flag -> virtual_flag -> type_expr -> class_signature -> unit
+
+type add_instance_variable_failure =
+  | Mutability_mismatch of mutable_flag
+  | Type_mismatch of Errortrace.unification_error
+
+exception Add_instance_variable_failed of add_instance_variable_failure
+
+val add_instance_variable : strict:bool -> Env.t ->
+  label -> mutable_flag -> virtual_flag -> type_expr -> class_signature -> unit
+
+type inherit_class_signature_failure =
+  | Self_type_mismatch of Errortrace.unification_error
+  | Method of label * add_method_failure
+  | Instance_variable of label * add_instance_variable_failure
+
+exception Inherit_class_signature_failed of inherit_class_signature_failure
+
+val inherit_class_signature : strict:bool -> Env.t ->
+  class_signature -> class_signature -> unit
+
+val update_class_signature :
+  Env.t -> class_signature -> label list * label list
+
+val hide_private_methods : Env.t -> class_signature -> unit
+
 exception Nondep_cannot_erase of Ident.t
 
 val nondep_type: Env.t -> Ident.t list -> type_expr -> type_expr
@@ -355,9 +384,13 @@ val nondep_cltype_declaration:
 val is_contractive: Env.t -> Path.t -> bool
 val normalize_type: type_expr -> unit
 
-val closed_schema: Env.t -> type_expr -> bool
+val nongen_schema: Env.t -> type_expr -> bool
         (* Check whether the given type scheme contains no non-generic
            type variables *)
+
+val nongen_class_declaration: class_declaration -> bool
+        (* Check whether the given class type contains no non-generic
+           type variables. Uses the empty environment.  *)
 
 val free_variables: ?env:Env.t -> type_expr -> type_expr list
         (* If env present, then check for incomplete definitions too *)
@@ -369,10 +402,7 @@ val closed_class:
         (* Check whether all type variables are bound *)
 
 val unalias: type_expr -> type_expr
-val signature_of_class_type: class_type -> class_signature
-val self_type: class_type -> type_expr
-val self_type_row: class_type -> type_expr
-val class_type_arity: class_type -> int
+
 val arity: type_expr -> int
         (* Return the arity (as for curried functions) of the given type. *)
 

typing/printtyp.ml
@@ -1557,7 +1557,7 @@ let tree_of_method mode (lab, priv, virt, ty) =
 
 let rec prepare_class_type params = function
   | Cty_constr (_p, tyl, cty) ->
-      let row = Ctype.self_type_row cty in
+      let row = Btype.self_type_row cty in
       if List.memq (proxy row) !visited_objects
       || not (List.for_all is_Tvar params)
       || List.exists (deep_occur row) tyl
@@ -1577,7 +1577,7 @@ let rec prepare_class_type params = function
 let rec tree_of_class_type mode params =
   function
   | Cty_constr (p', tyl, cty) ->
-      let row = Ctype.self_type_row cty in
+      let row = Btype.self_type_row cty in
       if List.memq (proxy row) !visited_objects
       || not (List.for_all is_Tvar params)
       then
@@ -1661,7 +1661,7 @@ let tree_of_class_declaration id cl rs =
   reset_except_context ();
   List.iter add_alias params;
   prepare_class_type params cl.cty_type;
-  let px = proxy (Ctype.self_type_row cl.cty_type) in
+  let px = proxy (Btype.self_type_row cl.cty_type) in
   List.iter mark_loops params;
 
   List.iter Names.check_name_of_type (List.map proxy params);
@@ -1683,13 +1683,13 @@ let tree_of_cltype_declaration id cl rs =
   reset_except_context ();
   List.iter add_alias params;
   prepare_class_type params cl.clty_type;
-  let px = proxy (Ctype.self_type_row cl.clty_type) in
+  let px = proxy (Btype.self_type_row cl.clty_type) in
   List.iter mark_loops params;
 
   List.iter Names.check_name_of_type (List.map proxy params);
   if is_aliased_proxy px then Names.check_name_of_type px;
 
-  let sign = Ctype.signature_of_class_type cl.clty_type in
+  let sign = Btype.signature_of_class_type cl.clty_type in
   let has_virtual_vars =
     Vars.fold (fun _ (_,vr,_) b -> vr = Virtual || b)
       sign.csig_vars false