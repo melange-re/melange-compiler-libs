melange-re/melange-compiler-libs

commit 93cf173c6c5d3e370908051d14ec7f179cd4757b

Add [@poll error] attribute (#10462)
(cherry picked from commit edf0075888ec27a50c4f50f76dba46f730797651)

lambda/translattribute.ml
@@ -42,6 +42,10 @@ let is_tmc_attribute = function
   | {txt=("tail_mod_cons"|"ocaml.tail_mod_cons")} -> true
   | _ -> false
 
+let is_poll_attribute = function
+  | {txt=("poll")} -> true
+  | _ -> false
+
 let find_attribute p attributes =
   let inline_attribute, other_attributes =
     List.partition (fun a -> p a.Parsetree.attr_name) attributes
@@ -176,6 +180,18 @@ let parse_local_attribute attr =
         ]
         payload
 
+let parse_poll_attribute attr =
+  match attr with
+  | None -> Default_poll
+  | Some {Parsetree.attr_name = {txt; loc}; attr_payload = payload} ->
+      parse_id_payload txt loc
+        ~default:Default_poll
+        ~empty:Default_poll
+        [
+          "error", Error_poll;
+        ]
+        payload
+
 let get_inline_attribute l =
   let attr, _ = find_attribute is_inline_attribute l in
   parse_inline_attribute attr
@@ -188,11 +204,33 @@ let get_local_attribute l =
   let attr, _ = find_attribute is_local_attribute l in
   parse_local_attribute attr
 
+let get_poll_attribute l =
+  let attr, _ = find_attribute is_poll_attribute l in
+  parse_poll_attribute attr
+
 let check_local_inline loc attr =
   match attr.local, attr.inline with
   | Always_local, (Always_inline | Hint_inline | Unroll _) ->
       Location.prerr_warning loc
         (Warnings.Duplicated_attribute "local/inline")
+  | _ ->
+      ()
+
+let check_poll_inline loc attr =
+  match attr.poll, attr.inline with
+  | Error_poll, (Always_inline | Hint_inline | Unroll _) ->
+      Location.prerr_warning loc
+        (Warnings.Inlining_impossible
+          "[@poll error] is incompatible with inlining")
+  | _ ->
+      ()
+
+let check_poll_local loc attr =
+  match attr.poll, attr.local with
+  | Error_poll, Always_local ->
+      Location.prerr_warning loc
+        (Warnings.Inlining_impossible
+          "[@poll error] is incompatible with local function optimization")
   | _ ->
       ()
 
@@ -207,7 +245,7 @@ let add_inline_attribute expr loc attributes =
             (Warnings.Duplicated_attribute "inline")
       end;
       let attr = { attr with inline } in
-      check_local_inline loc attr;
+      check_poll_inline loc attr;
       Lfunction { funct with attr = attr }
   | expr, (Always_inline | Hint_inline | Never_inline | Unroll _) ->
       Location.prerr_warning loc
@@ -243,6 +281,7 @@ let add_local_attribute expr loc attributes =
       end;
       let attr = { attr with local } in
       check_local_inline loc attr;
+      check_poll_local loc attr;
       Lfunction { funct with attr }
   | expr, (Always_local | Never_local) ->
       Location.prerr_warning loc
@@ -265,6 +304,26 @@ let add_tmc_attribute expr loc attributes =
         expr
   else
     expr
+
+let add_poll_attribute expr loc attributes =
+  match expr, get_poll_attribute attributes with
+  | expr, Default_poll -> expr
+  | Lfunction({ attr = { stub = false } as attr } as funct), poll ->
+      begin match attr.poll with
+      | Default_poll -> ()
+      | Error_poll ->
+          Location.prerr_warning loc
+            (Warnings.Duplicated_attribute "error_poll")
+      end;
+      let attr = { attr with poll } in
+      check_poll_inline loc attr;
+      check_poll_local loc attr;
+      let attr = { attr with inline = Never_inline; local = Never_local } in
+      Lfunction { funct with attr }
+  | expr, Error_poll ->
+      Location.prerr_warning loc
+        (Warnings.Misplaced_attribute "error_poll");
+      expr
 
 (* Get the [@inlined] attribute payload (or default if not present).
    It also returns the expression without this attribute. This is
@@ -338,7 +397,8 @@ let get_tailcall_attribute e =
 let check_attribute e {Parsetree.attr_name = { txt; loc }; _} =
   match txt with
   | "inline" | "ocaml.inline"
-  | "specialise" | "ocaml.specialise" -> begin
+  | "specialise" | "ocaml.specialise"
+  | "poll" -> begin
       match e.exp_desc with
       | Texp_function _ -> ()
       | _ ->
@@ -380,5 +440,9 @@ let add_function_attributes lam loc attr =
   in
   let lam =
     add_tmc_attribute lam loc attr
+  in
+  let lam =
+    (* last because poll overrides inline and local *)
+    add_poll_attribute lam loc attr
   in
   lam

lambda/lambda.ml
@@ -247,6 +247,10 @@ type local_attribute =
   | Never_local (* [@local never] *)
   | Default_local (* [@local maybe] or no [@local] attribute *)
 
+type poll_attribute =
+  | Error_poll (* [@poll error] *)
+  | Default_poll (* no [@poll] attribute *)
+
 type function_kind = Curried | Tupled
 
 type let_kind = Strict | Alias | StrictOpt
@@ -266,6 +270,7 @@ type function_attribute = {
   inline : inline_attribute;
   specialise : specialise_attribute;
   local: local_attribute;
+  poll: poll_attribute;
   is_a_functor: bool;
   stub: bool;
   tmc_candidate: bool;
@@ -350,6 +355,7 @@ let default_function_attribute = {
   inline = Default_inline;
   specialise = Default_specialise;
   local = Default_local;
+  poll = Default_poll;
   is_a_functor = false;
   stub = false;
   tmc_candidate = false;

lambda/lambda.mli
@@ -229,6 +229,10 @@ type local_attribute =
   | Never_local (* [@local never] *)
   | Default_local (* [@local maybe] or no [@local] attribute *)
 
+type poll_attribute =
+  | Error_poll (* [@poll error] *)
+  | Default_poll (* no [@poll] attribute *)
+
 type function_kind = Curried | Tupled
 
 type let_kind = Strict | Alias | StrictOpt
@@ -252,6 +256,7 @@ type function_attribute = {
   inline : inline_attribute;
   specialise : specialise_attribute;
   local: local_attribute;
+  poll: poll_attribute;
   is_a_functor: bool;
   stub: bool;
   tmc_candidate: bool;
@@ -310,6 +315,7 @@ and lambda_switch =
     sw_numblocks: int;                  (* Number of tag block cases *)
     sw_blocks: (int * lambda) list;     (* Tag block cases *)
     sw_failaction : lambda option}      (* Action to take if failure *)
+
 and lambda_event =
   { lev_loc: scoped_location;
     lev_kind: lambda_event_kind;

lambda/printlambda.ml
@@ -467,7 +467,11 @@ let function_attribute ppf t =
   | Never_local -> fprintf ppf "never_local@ "
   end;
   if t.tmc_candidate then
-    fprintf ppf "tail_mod_cons@ "
+    fprintf ppf "tail_mod_cons@ ";
+  begin match t.poll with
+  | Default_poll -> ()
+  | Error_poll -> fprintf ppf "error_poll@ "
+  end
 
 let apply_tailcall_attribute ppf = function
   | Default_tailcall -> ()

lambda/translmod.ml
@@ -491,6 +491,7 @@ let rec compile_functor ~scopes mexp coercion root_path loc =
       inline = inline_attribute;
       specialise = Default_specialise;
       local = Default_local;
+      poll = Default_poll;
       is_a_functor = true;
       stub = false;
       tmc_candidate = false;

commit af9ee6c92fd1f69491b1bbfa913838a5d885be19

fix a minor regression from #10462
See https://github.com/ocaml/ocaml/pull/10462#discussion_r802457480

lambda/translattribute.ml
@@ -245,6 +245,7 @@ let add_inline_attribute expr loc attributes =
             (Warnings.Duplicated_attribute "inline")
       end;
       let attr = { attr with inline } in
+      check_local_inline loc attr;
       check_poll_inline loc attr;
       Lfunction { funct with attr = attr }
   | expr, (Always_inline | Hint_inline | Never_inline | Unroll _) ->