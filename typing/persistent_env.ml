b6db8fa122cd21b17d23e7bc801366d91b40a03f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-05-27 20:19:56 -0700

    Remove mentions of unsafe_string

--- typing/persistent_env.ml
+++ typing/persistent_env.ml
@@ -313,7 +313,6 @@ let make_cmi penv modname sign alerts =
     List.concat [
       if !Clflags.recursive_types then [Cmi_format.Rectypes] else [];
       if !Clflags.opaque then [Cmi_format.Opaque] else [];
-      (if !Clflags.unsafe_string then [Cmi_format.Unsafe_string] else []);
       [Alerts alerts];
     ]
   in
