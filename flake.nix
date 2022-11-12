9c720e9833146f6d581c7c35ec052872ec9f280c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-11-12 13:54:55 -0500

    Fix nix build (#5)

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "melange-compiler-libs Nix Flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
+  inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
+          ocamlPackages = super.ocaml-ng.ocamlPackages_4_14;
+        });
+      in
+      rec {
+        packages.default = pkgs.callPackage ./nix { };
+        devShell = pkgs.callPackage ./nix/shell.nix { };
+      });
+}
