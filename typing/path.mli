4281ad9f1c2eb8ed754e5c285b95c39b324129a0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-22 18:37:49 -0700

    Make the distribution independent of compiler version

--- typing/path.mli
+++ typing/path.mli
@@ -4,7 +4,7 @@
 (*                                                                        *)
 (*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
 (*                                                                        *)
-(*   Copyright 2005 Institut National de Recherche en Informatique et     *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
 (*     en Automatique.                                                    *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
@@ -13,7 +13,40 @@
 (*                                                                        *)
 (**************************************************************************)
 
-val record_event_when_debug : bool ref
-val dont_record_crc_unit : string option ref
-val bs_gentype : string option ref
-val no_assert_false : bool ref
+(* Access paths *)
+
+type t =
+    Pident of Ident.t
+  | Pdot of t * string
+  | Papply of t * t
+
+val same: t -> t -> bool
+val compare: t -> t -> int
+val find_free_opt: Ident.t list -> t -> Ident.t option
+val exists_free: Ident.t list -> t -> bool
+val scope: t -> int
+val flatten : t -> [ `Contains_apply | `Ok of Ident.t * string list ]
+
+val name: ?paren:(string -> bool) -> t -> string
+    (* [paren] tells whether a path suffix needs parentheses *)
+val head: t -> Ident.t
+
+val print: Format.formatter -> t -> unit
+
+val heads: t -> Ident.t list
+
+val last: t -> string
+
+val is_uident: string -> bool
+
+type typath =
+  | Regular of t
+  | Ext of t * string
+  | LocalExt of Ident.t
+  | Cstr of t * string
+
+val constructor_typath: t -> typath
+val is_constructor_typath: t -> bool
+
+module Map : Map.S with type key = t
+module Set : Set.S with type elt = t
--- utils/lazy_backtrack.mli
+++ utils/lazy_backtrack.mli
@@ -2,9 +2,9 @@
 (*                                                                        *)
 (*                                 OCaml                                  *)
 (*                                                                        *)
-(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                   Fabrice Le Fessant, INRIA Saclay                     *)
 (*                                                                        *)
-(*   Copyright 2005 Institut National de Recherche en Informatique et     *)
+(*   Copyright 2012 Institut National de Recherche en Informatique et     *)
 (*     en Automatique.                                                    *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
@@ -13,7 +13,22 @@
 (*                                                                        *)
 (**************************************************************************)
 
-val record_event_when_debug : bool ref
-val dont_record_crc_unit : string option ref
-val bs_gentype : string option ref
-val no_assert_false : bool ref
+type ('a,'b) t
+
+type log
+
+val force : ('a -> 'b) -> ('a,'b) t -> 'b
+val create : 'a -> ('a,'b) t
+val get_arg : ('a,'b) t -> 'a option
+val get_contents : ('a,'b) t -> ('a,'b) Either.t
+val create_forced : 'b -> ('a, 'b) t
+val create_failed : exn -> ('a, 'b) t
+
+(* [force_logged log f t] is equivalent to [force f t] but if [f]
+   returns [Error _] then [t] is recorded in [log]. [backtrack log]
+   will then reset all the recorded [t]s back to their original
+   state. *)
+val log : unit -> log
+val force_logged :
+  log -> ('a -> ('b, 'c) result) -> ('a,('b, 'c) result) t -> ('b, 'c) result
+val backtrack : log -> unit
