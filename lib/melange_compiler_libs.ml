c67a0628de28b0e3971fb411481654915b758ebf
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-06-03 16:12:31 -0700

    feat: delete the old BuckleScript conditional compilation support

--- lib/melange_compiler_libs.ml
+++ lib/melange_compiler_libs.ml
@@ -1,15 +1,20 @@
-include Parsing0
-
 (* can be roughly generated with the following script:
    ls _build/default/lib | grep \.mli | cut -f1 -d'.' | \
      awk '{ myvar = toupper( substr( $0, 1, 1 ) ) substr( $0, 2 ); print "module " myvar " = " myvar; }'
 *)
+
 module Annot = Annot
+module Bs_clflags = Bs_clflags
 module Btype = Btype
+module Builtin_attributes = Builtin_attributes
 module Cmi_format = Cmi_format
 module Cmt_format = Cmt_format
+module Config = Config
+module Consistbl = Consistbl
 module Ctype = Ctype
 module Datarepr = Datarepr
+module Diffing = Diffing
+module Diffing_with_keys = Diffing_with_keys
 module Env = Env
 module Envaux = Envaux
 module Errortrace = Errortrace
@@ -18,18 +23,17 @@ module Includecore = Includecore
 module Includemod = Includemod
 module Includemod_errorprinter = Includemod_errorprinter
 module Lambda = Lambda
-module Lexer = Lexer
+module Lazy_backtrack = Lazy_backtrack
+module Load_path = Load_path
+module Location = Location
 module Matching = Matching
 module Mtype = Mtype
 module Oprint = Oprint
 module Outcometree = Outcometree
 module Parmatch = Parmatch
-module Parse = Parse
-module Parser = Parser
 module Path = Path
 module Patterns = Patterns
 module Persistent_env = Persistent_env
-module Pprintast = Pprintast
 module Predef = Predef
 module Primitive = Primitive
 module Printlambda = Printlambda
@@ -37,7 +41,6 @@ module Printpat = Printpat
 module Printtyp = Printtyp
 module Printtyped = Printtyped
 module Rec_check = Rec_check
-module Rescript_cpp = Rescript_cpp
 module Shape = Shape
 module Signature_group = Signature_group
 module Simplif = Simplif
@@ -68,3 +71,4 @@ module Typeopt = Typeopt
 module Types = Types
 module Typetexp = Typetexp
 module Untypeast = Untypeast
+module Warnings = Warnings
