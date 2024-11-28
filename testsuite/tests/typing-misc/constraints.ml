7e3e7e8fde404db3f754392d4470f98f0a47d493
Florian Angeletti <florian.angeletti@inria.fr>
2024-11-28 15:44:54 +0100

    Merge pull request #13510 from COCTI/fix13501
    Fix regression on mutually recursive types caused by #12180
    
    (cherry picked from commit 5339c86ff2b3e075654b318eb108701bc9134535)

--- testsuite/tests/typing-misc/constraints.ml
+++ testsuite/tests/typing-misc/constraints.ml
@@ -419,7 +419,7 @@ Error: The type abbreviation "cycle" is cyclic:
          "cycle id" = "cycle"
 |}]
 
-(* Vanishing constraints may be discarded during the translation *)
+(* Vanishing constraints should be checked during the translation *)
 type 'a t = [`Foo]
 type 'a cstr constraint 'a = float
 [%%expect{|
@@ -430,8 +430,11 @@ type 'a cstr constraint 'a = float
 type s = int
 and r = [s cstr t | `Bar]
 [%%expect{|
-type s = int
-and r = [ `Bar | `Foo ]
+Line 1, characters 0-12:
+1 | type s = int
+    ^^^^^^^^^^^^
+Error: This type constructor expands to type "s" = "int"
+       but is used here with type "float"
 |}]
 
 
@@ -447,4 +450,16 @@ Error: Constraints are not satisfied in this type.
        Type "'a foo" should be an instance of "int"
        Type "foo" was considered abstract when checking constraints in this
        recursive type definition.
+|}]
+
+(* PR#13510 *)
+
+type 'a x = [ `X of 'e ] constraint 'a = 'e list
+
+type p = private [> a x]
+and a = int list
+[%%expect{|
+type 'a x = [ `X of 'e ] constraint 'a = 'e list
+type p = private [> a x ]
+and a = int list
 |}]
