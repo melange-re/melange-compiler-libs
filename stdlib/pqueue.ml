ocaml/ocaml

stdlib: new module Pqueue

stdlib/pqueue.ml
@@ -1,0 +1,202 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Jean-Christophe Filliâtre                        *)
+(*                                                                        *)
+(*   Copyright 2023 CNRS                                                  *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* Priority queues over ordered types *)
+
+module type OrderedType =
+  sig
+    type t
+    val compare: t -> t -> int
+  end
+
+module type S =
+  sig
+    type elt
+    type t
+    val create: unit -> t
+    val length: t -> int
+    val is_empty: t -> bool
+    val add: t -> elt -> unit
+    val add_seq: t -> elt Seq.t -> unit
+    exception Empty
+    val min_elt: t -> elt
+    val min_elt_opt: t -> elt option
+    val pop_min: t -> elt
+    val pop_min_opt: t -> elt option
+    val remove_min: t -> unit
+    val clear: t -> unit
+    val copy: t -> t
+    val of_array: elt array -> t
+    val of_list: elt list -> t
+    val of_seq: elt Seq.t -> t
+    val iter: (elt -> unit) -> t -> unit
+    val fold: ('acc -> elt -> 'acc) -> 'acc -> t -> 'acc
+    val to_seq: t -> elt Seq.t
+  end
+
+module Make(Ord: OrderedType) =
+  struct
+    type elt = Ord.t
+
+    (* Our priority queues are implemented using the standard "min heap"
+       data structure, a dynamic array representing a binary tree. *)
+    type t = elt Dynarray.t
+
+    let create =
+      Dynarray.create
+
+    let length =
+      Dynarray.length
+
+    let is_empty h =
+      length h = 0
+
+    let clear =
+      Dynarray.clear
+
+    (* The node at index [i] has children nodes at indices [2 * i + 1]
+       and [2 * i + 2] -- if they are valid indices in the dynarray. *)
+    let left_child i = 2 * i + 1
+    let right_child i = 2 * i + 2
+    let parent_node i = (i - 1) / 2
+
+    (* We say that a heap respects the "heap ordering" if the value of
+       each node is no greater than the value of its children. The
+       algorithm manipulates arrays that respect the heap algorithm,
+       except for one node whose value may be too small or too large.
+
+       The auxiliary functions [sift_up] and [sift_down] take
+       such a misplaced value, and move it "up" (respectively: "down")
+       until the heap ordering is restored.
+
+       Functions [sift_up] and [sift_down] do not perform swaps, but
+       rather expect the value to be assigned in the heap as an
+       additional parameter [x], resulting in twice less assignments. *)
+
+    (* store [x] at index [i], moving it up if necessary *)
+    let rec sift_up h i x =
+      if i = 0 then Dynarray.set h 0 x else
+      let p = parent_node i in
+      let y = Dynarray.get h p in
+      if Ord.compare x y < 0 then (
+        Dynarray.set h i y;
+        sift_up h p x
+      ) else
+        Dynarray.set h i x
+
+    let add h x =
+      let i = Dynarray.length h in
+      Dynarray.add_last h x;
+      if i > 0 then sift_up h i x
+
+    let add_seq h s =
+      Seq.iter (add h) s
+
+    exception Empty
+
+    let min_elt h =
+      if Dynarray.length h = 0 then raise Empty;
+      Dynarray.get h 0
+
+    let min_elt_opt h =
+      if Dynarray.length h = 0 then None else Some (Dynarray.get h 0)
+
+    let lt h i j =
+      Ord.compare (Dynarray.get h i) (Dynarray.get h j) < 0
+
+    (* store [x] at index [i], moving it down if necessary *)
+    let rec sift_down h ~len i x =
+      let left = left_child i in
+      if left >= len then Dynarray.set h i x (* no child, stop *) else
+      let smallest =
+        let right = right_child i in
+        if right >= len then left (* no right child *) else
+        if lt h left right then left else right
+      in
+      let y = Dynarray.get h smallest in
+      if Ord.compare y x < 0 then (
+        Dynarray.set h i y;
+        sift_down h ~len smallest x
+      ) else
+        Dynarray.set h i x
+
+    let pop_min h =
+      let n = Dynarray.length h in
+      if n = 0 then raise Empty;
+      let x = Dynarray.pop_last h in
+      if n = 1 then x else (
+        let r = Dynarray.get h 0 in
+        sift_down h ~len:(n - 1) 0 x;
+        r
+      )
+
+    let pop_min_opt h =
+      let n = Dynarray.length h in
+      if n = 0 then None else
+      let x = Dynarray.pop_last h in
+      if n = 1 then Some x else (
+        let r = Dynarray.get h 0 in
+        sift_down h ~len:(n - 1) 0 x;
+        Some r
+      )
+
+    let remove_min h =
+      let n = Dynarray.length h in
+      if n = 0 then raise Empty;
+      let x = Dynarray.pop_last h in
+      if n > 1 then sift_down h ~len:(n - 1) 0 x
+
+    let copy =
+      Dynarray.copy
+
+    (* array to heap in linear time (Floyd, 1964)
+       many elements travel a short distance, few travel longer distances
+       and we can show that it adds to O(N) *)
+    let heapify h =
+      let n = Dynarray.length h in
+      for i = n/2 - 1 downto 0 do
+        sift_down h ~len:n i (Dynarray.get h i)
+      done;
+      h
+
+    let of_array a =
+      Dynarray.of_array a |> heapify
+
+    let of_list l =
+      Dynarray.of_list l |> heapify
+
+    let of_seq s =
+      Dynarray.of_seq s |> heapify
+
+    let iter =
+      Dynarray.iter
+
+    let fold =
+      Dynarray.fold_left
+
+    let to_seq =
+      Dynarray.to_seq
+
+  end
+
+(* Notes:
+
+  - Too bad [Dynarray.unsafe_get/set] are not exported.
+
+  - The exception [Empty] is declared in signature [S].
+    But it could also be a single, global exception in module [Pqueue].
+
+  - [fold] is [fold_left] to be consitent with module [Queue]
+
+*)

stdlib/pqueue.mli
@@ -1,0 +1,169 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Jean-Christophe Filliâtre                        *)
+(*                                                                        *)
+(*   Copyright 2023 CNRS                                                  *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Priority queues.
+
+    The {!Pqueue} module implements a data structure of priority queues,
+    given a total ordering function over the elements. This is a mutable
+    data structure. Smaller elements are considered to have a higher
+    priority (i.e., these are min-priority queues).
+
+    The implementation uses a heap stored in a dynamic array, and is
+    therefore reasonably efficient: accessing the minimum element
+    takes constant time, and insertion and removal take time
+    logarithmic in the size of the priority queue. Note that
+    [of_array] runs in linear time (and thus must be preferred to
+    repeated insertions with [add]).
+
+    It is fine to have several elements that are considered equal by
+    the ordering function and which are then considered as having the
+    same priority. Said otherwise, the priority queue must be seen as
+    a bag, not a set.  In particular, it is fine (and even expected)
+    to have elements of a tuple type such as [priority * whatever]
+    where the ordering function is only comparing the first component
+    of the elements.
+
+    It is guaranteed that the element returned by [min_elt] (or
+    [min_elt_opt]) is the one that is removed from the priority queue
+    by [pop_min] (or [pop_min_opt] or [remove_min]). This is important
+    in many algorithms, (e.g. when peeking at several priority queues
+    and then selecting one to remove from).
+
+    @since 5.2
+*)
+
+module type OrderedType =
+  sig
+    type t
+      (** The type of the priority queue elements. *)
+
+    val compare : t -> t -> int
+      (** A total ordering function over the elements.
+
+          This is a two-argument function [f] such that [f e1 e2] is
+          zero if the elements [e1] and [e2] are equal (i.e.  having
+          the same priority), [f e1 e2] is strictly negative if [e1]
+          is smaller than [e2] (i.e.  [e1] has higher priority than
+          [e2]), and [f e1 e2] is strictly positive if [e1] is greater
+          than [e2] (i.e. [e1] has lower priority than [e2]).
+
+          The generic structural comparison function {!Stdlib.compare}
+          is a suitable ordering function when the elements are
+          identified with their priority (such as integers, strings,
+          etc.) and when the smaller the better. *)
+  end
+(** Input signature of the functor {!Make}. *)
+
+module type S =
+  sig
+
+    (** {1:pqueue Priority queues} *)
+
+    type elt
+    (** The type of the priority queue elements. *)
+
+    type t
+    (** The type of priority queues. *)
+
+    val create: unit -> t
+    (** Return a new priority queue, initially empty. *)
+
+    val length: t -> int
+    (** Return the number of elements in a priority queue. *)
+
+    val is_empty: t -> bool
+    (** [is_empty q] is [true] iff [q] is empty, that is, iff [length q = 0]. *)
+
+    val add: t -> elt -> unit
+    (** [add q x] adds the element [x] in the priority queue [q]. *)
+
+    val add_seq: t -> elt Seq.t -> unit
+    (** [add q s] adds the elements of [s] in the priority queue [q]. *)
+
+    exception Empty
+    (** Raised when {!min_elt}, {!pop_min} or {!remove_min} is applied
+        to an empty queue. *)
+
+    val min_elt: t -> elt
+    (** [min_elt q] returns an element in queue [q] with minimal priority,
+        without removing it from the queue,
+        or raises {!Empty} if the queue is empty. *)
+
+    val min_elt_opt: t -> elt option
+    (** [min_elt_opt q] returns an element in queue [q] with minimal priority,
+        without removing it from the queue, or returns [None] if the queue
+        is empty. *)
+
+    val pop_min: t -> elt
+    (** [pop_min q] removes and returns an element in queue [q] with
+        minimal priority, or raises {!Empty} if the queue is empty. *)
+
+    val pop_min_opt: t -> elt option
+    (** [pop_min_opt q] removes and returns an element in queue [q] with
+        minimal priority, or returns [None] if the queue is empty. *)
+
+    val remove_min: t -> unit
+    (** [pop_min_opt q] removes an element in queue [q] with
+        minimal priority, or raises {!Empty} if the queue is empty. *)
+
+    val clear: t -> unit
+    (** Discard all elements from a priority queue. *)
+
+    val copy: t -> t
+    (** [copy q] is a shallow copy of [q] (a new priority queue
+        containing the same elements as [q]). *)
+
+    (** {1:conversions Conversions from other data structures} *)
+
+    val of_array: elt array -> t
+    (** [of_array a] returns a new priority queue containing the
+        elements of array [a]. Runs in linear time. *)
+
+    val of_list: elt list -> t
+    (** [of_list l] returns a new priority queue containing the
+        elements of list [l]. Runs in linear time. *)
+
+    val of_seq: elt Seq.t -> t
+    (** [of_seq s] returns a new priority queue containing the
+        elements of sequence [s]. Runs in linear time. *)
+
+    (** {1:iteration Iteration}
+
+        The order in which the elements of a priority queue are
+        traversed is unspecified.
+
+        It is a programming error to mutate a priority queue (by
+        adding or removing elements) during an iteration of the queue.
+        Such an error may be detected and signaled by the backing dynamic
+        array implementation, but this is not guaranteed. *)
+
+    val iter: (elt -> unit) -> t -> unit
+    (** [iter f q] calls [f] on each element of [q] in some
+        unspecified order. *)
+
+    val fold: ('acc -> elt -> 'acc) -> 'acc -> t -> 'acc
+    (** [fold f accu q] is [(f (... (f (f accu x1) x2) ...) xn)]
+        where [x1,x2,...,xn] are the elements of [q] in some
+        unspecified order. *)
+
+    val to_seq: t -> elt Seq.t
+    (** [to_seq q] is the sequence of elements of [q] in some
+        unspecified order. *)
+
+  end
+(** Output signature of the functor {!Make}. *)
+
+module Make (Ord : OrderedType) : S with type elt = Ord.t
+(** Functor building an implementation of the priority queue structure
+    given a totally ordered type. *)

melange-re/melange-compiler-libs

stdlib/pqueue.ml
@@ -1,0 +1,202 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Jean-Christophe Filliâtre                        *)
+(*                                                                        *)
+(*   Copyright 2023 CNRS                                                  *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* Priority queues over ordered types *)
+
+module type OrderedType =
+  sig
+    type t
+    val compare: t -> t -> int
+  end
+
+module type S =
+  sig
+    type elt
+    type t
+    val create: unit -> t
+    val length: t -> int
+    val is_empty: t -> bool
+    val add: t -> elt -> unit
+    val add_seq: t -> elt Seq.t -> unit
+    exception Empty
+    val min_elt: t -> elt
+    val min_elt_opt: t -> elt option
+    val pop_min: t -> elt
+    val pop_min_opt: t -> elt option
+    val remove_min: t -> unit
+    val clear: t -> unit
+    val copy: t -> t
+    val of_array: elt array -> t
+    val of_list: elt list -> t
+    val of_seq: elt Seq.t -> t
+    val iter: (elt -> unit) -> t -> unit
+    val fold: ('acc -> elt -> 'acc) -> 'acc -> t -> 'acc
+    val to_seq: t -> elt Seq.t
+  end
+
+module Make(Ord: OrderedType) =
+  struct
+    type elt = Ord.t
+
+    (* Our priority queues are implemented using the standard "min heap"
+       data structure, a dynamic array representing a binary tree. *)
+    type t = elt Dynarray.t
+
+    let create =
+      Dynarray.create
+
+    let length =
+      Dynarray.length
+
+    let is_empty h =
+      length h = 0
+
+    let clear =
+      Dynarray.clear
+
+    (* The node at index [i] has children nodes at indices [2 * i + 1]
+       and [2 * i + 2] -- if they are valid indices in the dynarray. *)
+    let left_child i = 2 * i + 1
+    let right_child i = 2 * i + 2
+    let parent_node i = (i - 1) / 2
+
+    (* We say that a heap respects the "heap ordering" if the value of
+       each node is no greater than the value of its children. The
+       algorithm manipulates arrays that respect the heap ordering,
+       except for one node whose value may be too small or too large.
+
+       The auxiliary functions [sift_up] and [sift_down] take
+       such a misplaced value, and move it "up" (respectively: "down")
+       until the heap ordering is restored.
+
+       Functions [sift_up] and [sift_down] do not perform swaps, but
+       rather expect the value to be assigned in the heap as an
+       additional parameter [x], resulting in twice less assignments. *)
+
+    (* store [x] at index [i], moving it up if necessary *)
+    let rec sift_up h i x =
+      if i = 0 then Dynarray.set h 0 x else
+      let p = parent_node i in
+      let y = Dynarray.get h p in
+      if Ord.compare x y < 0 then (
+        Dynarray.set h i y;
+        sift_up h p x
+      ) else
+        Dynarray.set h i x
+
+    let add h x =
+      let i = Dynarray.length h in
+      Dynarray.add_last h x;
+      if i > 0 then sift_up h i x
+
+    let add_seq h s =
+      Seq.iter (add h) s
+
+    exception Empty
+
+    let min_elt h =
+      if Dynarray.length h = 0 then raise Empty;
+      Dynarray.get h 0
+
+    let min_elt_opt h =
+      if Dynarray.length h = 0 then None else Some (Dynarray.get h 0)
+
+    let lt h i j =
+      Ord.compare (Dynarray.get h i) (Dynarray.get h j) < 0
+
+    (* store [x] at index [i], moving it down if necessary *)
+    let rec sift_down h ~len i x =
+      let left = left_child i in
+      if left >= len then Dynarray.set h i x (* no child, stop *) else
+      let smallest =
+        let right = right_child i in
+        if right >= len then left (* no right child *) else
+        if lt h left right then left else right
+      in
+      let y = Dynarray.get h smallest in
+      if Ord.compare y x < 0 then (
+        Dynarray.set h i y;
+        sift_down h ~len smallest x
+      ) else
+        Dynarray.set h i x
+
+    let pop_min h =
+      let n = Dynarray.length h in
+      if n = 0 then raise Empty;
+      let x = Dynarray.pop_last h in
+      if n = 1 then x else (
+        let r = Dynarray.get h 0 in
+        sift_down h ~len:(n - 1) 0 x;
+        r
+      )
+
+    let pop_min_opt h =
+      let n = Dynarray.length h in
+      if n = 0 then None else
+      let x = Dynarray.pop_last h in
+      if n = 1 then Some x else (
+        let r = Dynarray.get h 0 in
+        sift_down h ~len:(n - 1) 0 x;
+        Some r
+      )
+
+    let remove_min h =
+      let n = Dynarray.length h in
+      if n = 0 then raise Empty;
+      let x = Dynarray.pop_last h in
+      if n > 1 then sift_down h ~len:(n - 1) 0 x
+
+    let copy =
+      Dynarray.copy
+
+    (* array to heap in linear time (Floyd, 1964)
+       many elements travel a short distance, few travel longer distances
+       and we can show that it adds to O(N) *)
+    let heapify h =
+      let n = Dynarray.length h in
+      for i = n/2 - 1 downto 0 do
+        sift_down h ~len:n i (Dynarray.get h i)
+      done;
+      h
+
+    let of_array a =
+      Dynarray.of_array a |> heapify
+
+    let of_list l =
+      Dynarray.of_list l |> heapify
+
+    let of_seq s =
+      Dynarray.of_seq s |> heapify
+
+    let iter =
+      Dynarray.iter
+
+    let fold =
+      Dynarray.fold_left
+
+    let to_seq =
+      Dynarray.to_seq
+
+  end
+
+(* Notes:
+
+  - Too bad [Dynarray.unsafe_get/set] are not exported.
+
+  - The exception [Empty] is declared in signature [S].
+    But it could also be a single, global exception in module [Pqueue].
+
+  - [fold] is [fold_left] to be consitent with module [Queue]
+
+*)

stdlib/pqueue.mli
@@ -1,0 +1,169 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Jean-Christophe Filliâtre                        *)
+(*                                                                        *)
+(*   Copyright 2023 CNRS                                                  *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Priority queues.
+
+    The {!Pqueue} module implements a data structure of priority queues,
+    given a total ordering function over the elements. This is a mutable
+    data structure. Smaller elements are considered to have a higher
+    priority (i.e., these are min-priority queues).
+
+    The implementation uses a heap stored in a dynamic array, and is
+    therefore reasonably efficient: accessing the minimum element
+    takes constant time, and insertion and removal take time
+    logarithmic in the size of the priority queue. Note that
+    [of_array] runs in linear time (and thus must be preferred to
+    repeated insertions with [add]).
+
+    It is fine to have several elements that are considered equal by
+    the ordering function and which are then considered as having the
+    same priority. Said otherwise, the priority queue must be seen as
+    a bag, not a set.  In particular, it is fine (and even expected)
+    to have elements of a tuple type such as [priority * whatever]
+    where the ordering function is only comparing the first component
+    of the elements.
+
+    It is guaranteed that the element returned by [min_elt] (or
+    [min_elt_opt]) is the one that is removed from the priority queue
+    by [pop_min] (or [pop_min_opt] or [remove_min]). This is important
+    in many algorithms, (e.g. when peeking at several priority queues
+    and then selecting one to remove from).
+
+    @since 5.2
+*)
+
+module type OrderedType =
+  sig
+    type t
+      (** The type of the priority queue elements. *)
+
+    val compare : t -> t -> int
+      (** A total ordering function over the elements.
+
+          This is a two-argument function [f] such that [f e1 e2] is
+          zero if the elements [e1] and [e2] are equal (i.e.  having
+          the same priority), [f e1 e2] is strictly negative if [e1]
+          is smaller than [e2] (i.e.  [e1] has higher priority than
+          [e2]), and [f e1 e2] is strictly positive if [e1] is greater
+          than [e2] (i.e. [e1] has lower priority than [e2]).
+
+          The generic structural comparison function {!Stdlib.compare}
+          is a suitable ordering function when the elements are
+          identified with their priority (such as integers, strings,
+          etc.) and when the smaller the better. *)
+  end
+(** Input signature of the functor {!Make}. *)
+
+module type S =
+  sig
+
+    (** {1:pqueue Priority queues} *)
+
+    type elt
+    (** The type of the priority queue elements. *)
+
+    type t
+    (** The type of priority queues. *)
+
+    val create: unit -> t
+    (** Return a new priority queue, initially empty. *)
+
+    val length: t -> int
+    (** Return the number of elements in a priority queue. *)
+
+    val is_empty: t -> bool
+    (** [is_empty q] is [true] iff [q] is empty, that is, iff [length q = 0]. *)
+
+    val add: t -> elt -> unit
+    (** [add q x] adds the element [x] in the priority queue [q]. *)
+
+    val add_seq: t -> elt Seq.t -> unit
+    (** [add q s] adds the elements of [s] in the priority queue [q]. *)
+
+    exception Empty
+    (** Raised when {!min_elt}, {!pop_min} or {!remove_min} is applied
+        to an empty queue. *)
+
+    val min_elt: t -> elt
+    (** [min_elt q] returns an element in queue [q] with minimal priority,
+        without removing it from the queue,
+        or raises {!Empty} if the queue is empty. *)
+
+    val min_elt_opt: t -> elt option
+    (** [min_elt_opt q] returns an element in queue [q] with minimal priority,
+        without removing it from the queue, or returns [None] if the queue
+        is empty. *)
+
+    val pop_min: t -> elt
+    (** [pop_min q] removes and returns an element in queue [q] with
+        minimal priority, or raises {!Empty} if the queue is empty. *)
+
+    val pop_min_opt: t -> elt option
+    (** [pop_min_opt q] removes and returns an element in queue [q] with
+        minimal priority, or returns [None] if the queue is empty. *)
+
+    val remove_min: t -> unit
+    (** [remove_min q] removes an element in queue [q] with
+        minimal priority, or raises {!Empty} if the queue is empty. *)
+
+    val clear: t -> unit
+    (** Discard all elements from a priority queue. *)
+
+    val copy: t -> t
+    (** [copy q] is a shallow copy of [q] (a new priority queue
+        containing the same elements as [q]). *)
+
+    (** {1:conversions Conversions from other data structures} *)
+
+    val of_array: elt array -> t
+    (** [of_array a] returns a new priority queue containing the
+        elements of array [a]. Runs in linear time. *)
+
+    val of_list: elt list -> t
+    (** [of_list l] returns a new priority queue containing the
+        elements of list [l]. Runs in linear time. *)
+
+    val of_seq: elt Seq.t -> t
+    (** [of_seq s] returns a new priority queue containing the
+        elements of sequence [s]. Runs in linear time. *)
+
+    (** {1:iteration Iteration}
+
+        The order in which the elements of a priority queue are
+        traversed is unspecified.
+
+        It is a programming error to mutate a priority queue (by
+        adding or removing elements) during an iteration of the queue.
+        Such an error may be detected and signaled by the backing dynamic
+        array implementation, but this is not guaranteed. *)
+
+    val iter: (elt -> unit) -> t -> unit
+    (** [iter f q] calls [f] on each element of [q] in some
+        unspecified order. *)
+
+    val fold: ('acc -> elt -> 'acc) -> 'acc -> t -> 'acc
+    (** [fold f accu q] is [(f (... (f (f accu x1) x2) ...) xn)]
+        where [x1,x2,...,xn] are the elements of [q] in some
+        unspecified order. *)
+
+    val to_seq: t -> elt Seq.t
+    (** [to_seq q] is the sequence of elements of [q] in some
+        unspecified order. *)
+
+  end
+(** Output signature of the functor {!Make}. *)
+
+module Make (Ord : OrderedType) : S with type elt = Ord.t
+(** Functor building an implementation of the priority queue structure
+    given a totally ordered type. *)