84061af5044161b227ed5bda9bf9df06a825366c
samsa1 <samuel.aime.vivien@free.fr>
2025-03-05 15:38:20 +0100

    Lift inlined record out of Tpackage as a new type

--- typing/types.ml
+++ typing/types.ml
@@ -44,8 +44,11 @@ and type_desc =
   | Tvariant of row_desc
   | Tunivar of string option
   | Tpoly of type_expr * type_expr list
-  | Tpackage of
-      { pack_path : Path.t; pack_cstrs : (string list * type_expr) list }
+  | Tpackage of package
+
+and package =
+    { pack_path : Path.t;
+      pack_cstrs : (string list * type_expr) list }
 
 and row_desc =
     { row_fields: (label * row_field) list;
