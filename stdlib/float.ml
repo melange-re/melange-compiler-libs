b5f2f49b6437b663e28c3a36303cd9b7b0a1b3b5
Gabriel Scherer <gabriel.scherer@gmail.com>
2025-03-10 18:14:07 +0100

    Float.Array.fill: add [@inline] attribute to expose float-unboxing opportunity
    Reported-by: Alain Frisch <alain@frisch.fr>

--- stdlib/float.ml
+++ stdlib/float.ml
@@ -271,7 +271,8 @@ module Array = struct
     else if length a2 = 0 then unsafe_sub a1 0 l1
     else append_prim a1 a2
 
-  let fill a ofs len v =
+  (* inlining exposes a float-unboxing opportunity for [v] *)
+  let[@inline] fill a ofs len v =
     check a ofs len "Float.Array.fill";
     unsafe_fill a ofs len v
 
