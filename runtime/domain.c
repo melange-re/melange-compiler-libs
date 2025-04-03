gasche/ocaml

runtime: count the work suspended by each ramp-up phase

runtime/domain.c
@@ -734,6 +734,7 @@ static void domain_create(uintnat initial_minor_heap_wsize,
   domain_state->allocated_words_direct = 0;
   domain_state->allocated_words_suspended = 0;
   domain_state->allocated_words_resumed = 0;
+  domain_state->current_rampup_allocated_words_diff = 0;
   domain_state->swept_words = 0;
 
   domain_state->local_roots = NULL;

runtime/gc_ctrl.c
@@ -422,11 +422,24 @@ CAMLprim value caml_ml_runtime_warnings_enabled(value unit)
 
 /* Ramp-up phase. */
 
-caml_result caml_gc_ramp_up(value callback) {
+caml_result caml_gc_ramp_up(value callback, uintnat *out_suspended_words) {
     /* Set the GC policy to ramp-up. */
     Caml_state->gc_policy = (Caml_state->gc_policy | CAML_GC_RAMP_UP);
 
+    /* Start a new rampup phase: the sum
+         [current_rampup_allocated_words_diff + allocated_words_suspended]
+       must be 0. */
+    Caml_state->current_rampup_allocated_words_diff =
+      0 - Caml_state->allocated_words_suspended;
+
     caml_result res = caml_callback_res(callback, Val_unit);
+
+    /* Count the suspended allocation for the phase. */
+    intnat suspended_words =
+      Caml_state->current_rampup_allocated_words_diff
+      + Caml_state->allocated_words_suspended;
+
+    *out_suspended_words = suspended_words;
 
     Caml_state->gc_policy = (Caml_state->gc_policy & ~CAML_GC_RAMP_UP);
 

runtime/major_gc.c
@@ -736,6 +736,8 @@ update_major_slice_work(intnat howmuch,
   my_extra_count = dom_st->extra_heap_resources;
 
   dom_st->stat_major_words += dom_st->allocated_words;
+  dom_st->current_rampup_allocated_words_diff +=
+    dom_st->allocated_words_suspended;
 
   dom_st->allocated_words = 0;
   dom_st->allocated_words_direct = 0;
@@ -2154,6 +2156,8 @@ void caml_finish_marking (void)
     caml_empty_mark_stack();
     caml_shrink_mark_stack();
     Caml_state->stat_major_words += Caml_state->allocated_words;
+    Caml_state->current_rampup_allocated_words_diff +=
+      Caml_state->allocated_words_suspended;
     Caml_state->allocated_words = 0;
     Caml_state->allocated_words_direct = 0;
     Caml_state->allocated_words_suspended = 0;

melange-re/melange-compiler-libs

runtime/domain.c
@@ -734,6 +734,7 @@ static void domain_create(uintnat initial_minor_heap_wsize,
   domain_state->allocated_words_direct = 0;
   domain_state->allocated_words_suspended = 0;
   domain_state->allocated_words_resumed = 0;
+  domain_state->current_ramp_up_allocated_words_diff = 0;
   domain_state->swept_words = 0;
 
   domain_state->local_roots = NULL;

runtime/gc_ctrl.c
@@ -422,11 +422,24 @@ CAMLprim value caml_ml_runtime_warnings_enabled(value unit)
 
 /* Ramp-up phase. */
 
-caml_result caml_gc_ramp_up(value callback) {
+caml_result caml_gc_ramp_up(value callback, uintnat *out_suspended_words) {
     /* Set the GC policy to ramp-up. */
     Caml_state->gc_policy = (Caml_state->gc_policy | CAML_GC_RAMP_UP);
 
+    /* Start a new ramp_up phase: the sum
+         [current_ramp_up_allocated_words_diff + allocated_words_suspended]
+       must be 0. */
+    Caml_state->current_ramp_up_allocated_words_diff =
+      0 - Caml_state->allocated_words_suspended;
+
     caml_result res = caml_callback_res(callback, Val_unit);
+
+    /* Count the suspended allocation for the phase. */
+    intnat suspended_words =
+      Caml_state->current_ramp_up_allocated_words_diff
+      + Caml_state->allocated_words_suspended;
+
+    *out_suspended_words = suspended_words;
 
     Caml_state->gc_policy = (Caml_state->gc_policy & ~CAML_GC_RAMP_UP);
 

runtime/major_gc.c
@@ -736,6 +736,8 @@ update_major_slice_work(intnat howmuch,
   my_extra_count = dom_st->extra_heap_resources;
 
   dom_st->stat_major_words += dom_st->allocated_words;
+  dom_st->current_ramp_up_allocated_words_diff +=
+    dom_st->allocated_words_suspended;
 
   dom_st->allocated_words = 0;
   dom_st->allocated_words_direct = 0;
@@ -2155,6 +2157,8 @@ void caml_finish_marking (void)
     caml_empty_mark_stack();
     caml_shrink_mark_stack();
     Caml_state->stat_major_words += Caml_state->allocated_words;
+    Caml_state->current_ramp_up_allocated_words_diff +=
+      Caml_state->allocated_words_suspended;
     Caml_state->allocated_words = 0;
     Caml_state->allocated_words_direct = 0;
     Caml_state->allocated_words_suspended = 0;