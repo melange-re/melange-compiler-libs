gasche/ocaml

[refactor] runtime: auxiliary function for allocated_words update

runtime/caml/major_gc.h
@@ -57,6 +57,15 @@ void caml_orphan_finalisers(caml_domain_state*);
    so it need not be atomic */
 extern uintnat caml_major_cycles_completed;
 
+inline void caml_update_major_allocated_words(
+  caml_domain_state *self, intnat words, int direct
+) {
+  self->allocated_words += words;
+  if (direct) {
+    self->allocated_words_direct += words;
+  }
+}
+
 #endif /* CAML_INTERNALS */
 
 #endif /* CAML_MAJOR_GC_H */

melange-re/melange-compiler-libs

runtime/caml/major_gc.h
@@ -57,6 +57,15 @@ void caml_orphan_finalisers(caml_domain_state*);
    so it need not be atomic */
 extern uintnat caml_major_cycles_completed;
 
+Caml_inline void caml_update_major_allocated_words(
+  caml_domain_state *self, intnat words, int direct
+) {
+  self->allocated_words += words;
+  if (direct) {
+    self->allocated_words_direct += words;
+  }
+}
+
 #endif /* CAML_INTERNALS */
 
 #endif /* CAML_MAJOR_GC_H */