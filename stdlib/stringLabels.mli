9b4c8bb58b2dd715cf4f0694a7298593ae603574
Bozhidar Batsov <bozhidar@batsov.dev>
2026-02-18 10:01:30 +0200

    Fix typos in stdlib documentation (#14560)
    - string.mli, stringLabels.mli: "deacreasing" -> "decreasing"
    - string.mli, stringLabels.mli: "Occurences" -> "Occurrences"
    - string.mli, stringLabels.mli: "multiple time" -> "multiple times"
    - format.mli: "compatility" -> "compatibility"
    
    (cherry picked from commit 3a89108218bb646988f58b461edf6a566220159b)

--- stdlib/stringLabels.mli
+++ stdlib/stringLabels.mli
@@ -349,7 +349,7 @@ val rsplit_all :
 (** [rsplit_all ~sep s] is the list of all substrings of [s] that are
     delimited by non-overlapping matches of the separator [sep] or
     [[s]] if [sep] can't be found. Search for [sep] starts at position
-    [length s] in deacreasing indexing order and uses {!rfind_all}.
+    [length s] in decreasing indexing order and uses {!rfind_all}.
 
     Substrings [sub] for which [drop sub] is [true] are not included
     in the result. [drop] defaults to [Fun.const false].
@@ -603,7 +603,7 @@ val rfind_all :
 
 (** {1:replacing Replacing substrings}
 
-    {b Note.} To replace the same [sub] string multiple time, partially
+    {b Note.} To replace the same [sub] string multiple times, partially
     applying the [~sub] argument of these functions and using the
     resulting function repeatedly is more efficient. *)
 
@@ -639,7 +639,7 @@ val replace_all :
   ?start:int -> string -> string
 (** [replace_all ~sub ~by ~start s] replaces by [by] all non-overlapping
     occurrences of [sub] in [s] at or after the index or position [start]
-    (defaults to [0]). Occurences are found in increasing indexing order.
+    (defaults to [0]). Occurrences are found in increasing indexing order.
 
     If [sub] is [""], this inserts [by] on all positions from [start] on.
 
