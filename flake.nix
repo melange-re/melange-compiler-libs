016addf9f5586b2e3fdf4cfb394c65f748df01a3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-07-05 13:26:11 -0700

    build(nix): use correct branch-local OCaml version

--- flake.nix
+++ flake.nix
@@ -2,25 +2,33 @@
   description = "melange-compiler-libs Nix Flake";
   inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-      nixpkgsFor = forAllSystems (system:
-        nixpkgs.legacyPackages.${system}.extend (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
-        }));
+      nixpkgsFor = forAllSystems (
+        system:
+        nixpkgs.legacyPackages.${system}.extend (
+          self: super: {
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_4;
+          }
+        )
+      );
     in
 
     {
       overlays.default = import ./nix/overlay.nix;
-      packages =
-        (forAllSystems (system:
+      packages = (
+        forAllSystems (
+          system:
           let
             pkgs = nixpkgsFor.${system};
           in
           {
             default = pkgs.callPackage ./nix { };
-          }));
+          }
+        )
+      );
 
       devShells = forAllSystems (system: {
         default = nixpkgsFor.${system}.callPackage ./nix/shell.nix { };
